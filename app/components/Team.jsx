ddd12b80fb940cf41d8d9dd4d9f6839313cfadfa
aparaarachi8145 <aparaaarachi2006@gmail.com>
2024-12-17 22:26:48 +0530

    changes made

--- app/components/Team.jsx
+++ app/components/Team.jsx
@@ -3,49 +3,57 @@ import React from 'react'
 function Team() {
   return (
      
-    <div className="h-screen w-screen bg-white py-6 ">
-      <div className='flex items-center justify-center text-2xl text-bold'>ABOUT THE TEAM...</div> <br />
-      <div className="comtainer grid grid-cols-1 md:grid-cols-3 gap-5 max-w-5xl max-h-xl mx-auto pb-20 ">
+    <div className="w-screen bg-gray-100 py-6 "> 
+      <div className='flex item-center justify-center text-4xl text-bold text-first'>ABOUT THE TEAM...</div> <br />
+      <p className=' text-center mx-auto mb-6 '> <b>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br /> Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. <br /> Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien.</b>
+      </p>
+      <div className="comtainer grid grid-cols-1 md:grid-cols-3 gap-5 max-w-6xl max-h-xl mx-auto pb-20 ">
       {/* {card1} */}
       <Card
-        image="/sample.jpg"
-          description="Hello everyone."
-          name="AB HADLEY"
+        image="/doc1.jpg"
+          description="Hello everyone. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien.
+"
+          name="DR. ADIN AZAM"
           date="May 26">
         </Card>
         {/* {card2} */}
         <Card
-        image=""
-          description="Hello everyone."
-          name="AB HADLEY"
+        image="/doc2.jpg"
+          description="Hello everyone. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien.
+"
+          name="DR. ADIN AZAM"
           date="May 26">
         </Card>
         {/* {card3} */}
         <Card
-        image=""
-          description="Hello everyone."
-          name="AB HADLEY"
+        image="/doc3.jpg"
+          description="Hello everyone. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien.
+"
+          name="DR. ADIN AZAM"
           date="May 26">
         </Card>
         {/* {card4} */}
         <Card
-        image=""
-          description="Hello everyone."
-          name="AB HADLEY"
+        image="/doc4.jpg"
+          description="Hello everyone. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien.
+"
+          name="DR. ADIN AZAM"
           date="May 26">
         </Card>
         {/* {card5} */}
         <Card
-        image=""
-          description="Hello everyone."
-          name="AB HADLEY"
+        image="/doc5.jpg"
+          description="Hello everyone. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien.
+"
+          name="DR. ADIN AZAM"
           date="May 26">
         </Card>
         {/* {card6} */}
         <Card
-        image=""
-          description="Hello everyone."
-          name="AB HADLEY"
+        image="/doc6.jpg"
+          description="Hello everyone. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien.
+"
+          name="DR. ADIN AZAM"
           date="May 26">
         </Card>
       </div>
@@ -63,13 +71,14 @@ const Card = ({ image, description, name, date }) => {
         />
 </div>
       <div className="p-6">
-        <p className="text-black text-sm mb-4">{description}</p>
+        <p className="text-black text-sm mb-4 text-center">{description}</p>
       </div>
-      
-<div className="px-6 pb-3 text-black text-md font-bold flex justify-between items-center ">
+   <div className='m-5'> 
+<div className="px-6 pb-3 text-black text-md font-bold flex justify-between items-center border-t-2 ">
         <div>{name}</div>
         <div>{date}</div>
       </div>
+      </div>  
     </div>
   );;
 };
