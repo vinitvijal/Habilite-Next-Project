0de00ffc10d8aa8e171024c2d7e9c3a1fa0a2263
aparaarachi8145 <aparaaarachi2006@gmail.com>
2024-12-20 01:11:11 +0530

    final changes

--- app/components/Team.jsx
+++ app/components/Team.jsx
@@ -5,13 +5,13 @@ function Team() {
      
     <div className="w-screen bg-gray-100 py-6 "> 
       <div className='flex item-center justify-center text-4xl font-bold text-first'>ABOUT THE TEAM...</div> <br />
-      <p className=' text-center mx-auto mb-10 '> <b>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br /> Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. <br /> Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien.</b>
+      <p className=' text-center mx-auto mb-6'> <b>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br /> Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. <br /> Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien.</b>
       </p>
       <div className="comtainer grid grid-cols-1 md:grid-cols-2 gap-16 max-w-7xl max-h-xl mx-auto pb-20">
       {/* {card1} */}
       <Card
         image="/doc1.jpg"
-          description="Hello everyone. Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien.
+          description=" Hello everyone. Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien.
            Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien. "
           name="DR. ADIN AZAM"
           date="May 26">
@@ -19,7 +19,7 @@ function Team() {
         {/* {card2} */}
         <Card
         image="/doc2.jpg"
-          description="Hello everyone. Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien.
+          description=" Hello everyone. Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien.
            Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien. "
           name="DR. ADIN AZAM"
           date="May 26">
@@ -27,7 +27,7 @@ function Team() {
         {/* {card3} */}
         <Card
         image="/doc3.jpg"
-          description="Hello everyone. Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien.
+          description=" Hello everyone. Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien.
            Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien. "
           name="DR. ADIN AZAM"
           date="May 26">
@@ -35,7 +35,7 @@ function Team() {
         {/* {card4} */}
         <Card
         image="/doc4.jpg"
-          description="Hello everyone. Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien.
+          description=" Hello everyone. Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nam viverra bibendum eros, nec consectetur nulla feugiat eu. Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien.
            Duis sollicitudin metus vitae ipsum dignissim, a consequat sapien. "
           name="DR. ADIN AZAM"
           date="May 26">
@@ -56,7 +56,7 @@ const Card = ({ image, description, name, date }) => {
           className="w-full h-full object-cover"
         />
       </div>
-      <div className="bg-gray-100 p-6 text-sm mb-1 flex items-center justify-center text-center mt-2 transform hover:scale-105 transition-transform duration-300">
+      <div className="bg-gray-100 p-6 text-sm flex items-center justify-center text-center mt-2 transform hover:scale-105 transition-transform duration-300">
         <p className="text-black">{description}</p>
       </div>
      
