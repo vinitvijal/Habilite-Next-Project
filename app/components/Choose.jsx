6abbace5341d44fe878d571bf2906c4ffa2a41dd
tomatocode <adinazam70076@gmail.com>
2024-12-20 00:15:17 +0530

    Issues fixed

--- app/components/Choose.jsx
+++ app/components/Choose.jsx
@@ -3,25 +3,25 @@ import Image from "next/image";
 function Choose() {
   return (
     <div className="bg-gray-100 w-screen h-screen">
-      <div className="font-sans text-gray-800 mb-5 pt-20">
+      <div className="text-gray-800 mb-5 pt-20">
         {/* Header Section */}
         <div className="container mx-auto px-20 mb-5 text-center">
-          <h5 className="text-first text-sm  uppercase tracking-wide text-3xl">
+          <h5 className="text-first font-bold uppercase tracking-wide text-3xl">
             Why Choose Us? Dr Apara Arachi.
           </h5>
           <h1 className="text-second text-2xl font-bold mb-10">
             Compassionate care, experienced professionals, and state-of-the-art
             facilities <br /> —your health is our priority.
           </h1>
-          <p className="text-gray-500 text-sm mb-10">
+          <p className=" text-sm mb-16">
             Trusted care, personalized treatment, and a commitment to your
             well-being—because you deserve the best.
           </p>
         </div>
 
         <div className="container grid grid-cols-1 md:grid-cols-3 gap-10 max-w-3xl mx-auto pb-20 mx-auto">
-          <div className="text-center shadow-lg shadow-second p-6 cursor-pointer rounded-xl">
-            <div className="w-10 h-10 bg-gray-200 rounded-full flex items-center justify-center mx-auto mb-4 font-bold shadow-lg shadow-second">
+          <div className="text-center p-6 rounded-lg transform hover:-translate-y-10 transition-transform duration-300 bg-white shadow-xl cursor-pointer rounded-xl">
+            <div className="w-10 h-10 rounded-full flex items-center justify-center mx-auto mb-4 font-bold shadow-lg shadow-second">
               1
             </div>
             <h3 className="text-lg font-semibold mb-2">
@@ -35,8 +35,8 @@ function Choose() {
             </p>
           </div>
 
-          <div className="text-center p-6  shadow-lg shadow-second cursor-pointer rounded-xl">
-            <div className="w-10 h-10 bg-gray-200 rounded-full flex items-center justify-center mx-auto mb-4 font-bold shadow-lg shadow-second">
+          <div className="text-center p-6 bg-white rounded-lg transform hover:-translate-y-10 transition-transform duration-300 shadow-xl cursor-pointer rounded-xl">
+            <div className="w-10 h-10 rounded-full flex items-center justify-center mx-auto mb-4 font-bold shadow-lg shadow-second">
               2
             </div>
             <h3 className="text-lg font-semibold mb-2">
@@ -49,8 +49,8 @@ function Choose() {
               deliver top-notch care.
             </p>
           </div>
-          <div className="text-center p-6  shadow-lg shadow-second cursor-pointer rounded-xl ">
-            <div className="w-10 h-10 bg-gray-200 rounded-full flex items-center justify-center mx-auto mb-4 font-bold shadow-lg shadow-second">
+          <div className="text-center p-6 bg-white transform hover:-translate-y-10 transition-transform duration-300 shadow-xl cursor-pointer rounded-xl ">
+            <div className="w-10 h-10 rounded-full flex items-center justify-center mx-auto mb-4 font-bold shadow-lg shadow-second">
               3
             </div>
             <h3 className="text-lg font-semibold mb-2">
