d74c52db79ff869bc2fa4cb007be9c9f21309e87
Vinu <vineet.22566@sscbs.du.ac.in>
2024-12-20 20:58:56 +0530

    bug fixed and responsivity

--- app/components/Blog.jsx
+++ app/components/Blog.jsx
@@ -39,7 +39,7 @@ function Blog(){
     <div className="max-w-6xl mx-auto p-6">
       <h1 className="text-4xl font-bold text-black mb-6 pt-10 text-center">Recent Blogs</h1>
       <h1 className="text-xl font-semibold text-third mb-20 pb-4 text-center">Here's what we've been to lately </h1>
-      <div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-12 m-3">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 m-3">
         {blogs.map((blog) => (
           <div key={blog.id} className="bg-white shadow-md rounded-lg overflow-hidden hover:shadow-lg hover:scale-105 transition-transform duration-300">
             <img src={blog.imageUrl} alt={blog.title} className="w-full h-48 object-cover" />
