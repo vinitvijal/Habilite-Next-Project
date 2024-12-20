7fb4dd56a1a1b8f3cbf7ea948cf4bad68e4ba8ba
aparaarachi8145 <aparaaarachi2006@gmail.com>
2024-12-20 18:10:57 +0530

    done

--- app/components/Team.jsx
+++ app/components/Team.jsx
@@ -7,7 +7,7 @@ function Team() {
       <div className='flex items-center justify-center text-6xl text-first px-10'>Meet Our Team</div> <br />
       <p className=' text-second flex items-center justify-center text-center mx-auto mb-8 text-lg px-10'> <b>We are a team of qualified psychiatrists and psychologists from premier institutions of the country. <br />We are highly motivated to provide cutting egde evidence based management for mental health issues tailored to individual needs.</b>
       </p>
-      <div className="comtainer grid grid-cols-1 md:grid-cols-3 gap-12 w-full max-h-xl mx-auto p-10">
+      <div className="comtainer grid grid-cols-1 md:grid-cols-3 gap-12 w-full max-h-xl mx-auto p-12">
       {/* {card1} */}
       <Card
         image="/doc1.jpg"
@@ -41,19 +41,6 @@ function Team() {
           name="DR. ADIN AZAM"
           date="May 26">
         </Card>
-        {/* {card4} */}
-        {/* <Card
-        image="/doc4.jpg"
-        greeting="Hello everyone."
-        description="         
-        MBBS (MAMC), MD (Gold Medalist) Psychiatry (AIIMS, Delhi), DNB
-        Founder & Head, Habilite Clinic
-        Consultant Psychiatrist and Deaddiction Specialist "
-        goal="We are a team of doctors and clinical psychologists from the most premier institutes in the country. We offer end-to-end solutions for assessing and managing all mental-health related issues"
-          name="DR. ADIN AZAM"
-          date="May 26">
-        </Card> */}
-       
       </div>
     </div>
   )
@@ -61,7 +48,7 @@ function Team() {
 const Card = ({ image, greeting,description, name, date }) => {
   return (
     
-    <div className="bg-white cursor-pointer rounded-lg border-3 border-fourth p-3 border border-third hover:border-first hover:border-3 transform hover:scale-105 transition-transform duration-300">
+    <div className="bg-white cursor-pointer rounded-lg p-3 border-2 border-gray-50 hover:border-first hover:border-3 transform hover:scale-105 transition-transform duration-300">
 
       <div className="flex justify-center item-center transform hover:scale-105 transition-transform duration-300 w-full ">
         <img
@@ -69,13 +56,13 @@ const Card = ({ image, greeting,description, name, date }) => {
           className="w-80 h-80 rounded-full object-cover shadow-third shadow-lg"
         />
       </div>
-      <div className="bg-gray-100 p-4  gap-2 text-lg flex flex-col items-center justify-center text-center m-4">
+      <div className="bg-gray-100 p-4 gap-2 text-lg flex flex-col items-center justify-center text-center m-4">
       <p className="text-black">{greeting}</p>
         <p className="text-black">{description}</p>
       </div>
      
    <div className='m-3'> 
-<div className="p-3 gap-2 text-first text-lg font-bold flex justify-between items-center border-t-2 border-first transform hover:scale-105 transition-transform duration-300">
+<div className="p-5 gap-2 text-first text-lg font-bold flex justify-between items-center border-t-2 border-first transform hover:scale-105 transition-transform duration-300">
         <div >{name}</div>
         <div>{date}</div>
       </div>
