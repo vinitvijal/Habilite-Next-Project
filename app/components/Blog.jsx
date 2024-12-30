a79f337fcbafd71a3c51c4060833b9a7185922df
Namiiii-sys <Namitamehra207@gmail.com>
2024-12-30 19:29:55 +0530

    changes-done

--- app/components/Blog.jsx
+++ app/components/Blog.jsx
@@ -34,56 +34,50 @@ function Blog(){
     },
 
   ];
-  return (
-    <> 
-    <div className="max-w-6xl mx-auto p-6">
-      <h1 className="text-4xl font-bold text-black mb-6 pt-10 text-center">Recent Blogs</h1>
-      <h1 className="text-xl font-semibold text-third mb-20 pb-4 text-center">Here's what we've been to lately </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 m-3">
-        {blogs.map((blog) => (
-          <div key={blog.id} className="bg-white shadow-md rounded-lg overflow-hidden hover:shadow-lg hover:scale-105 transition-transform duration-300">
-            <img src={blog.imageUrl} alt={blog.title} className="w-full h-48 object-cover" />
-            <div className="p-4">
-
-              <h2 className="mt-4 text-lg font-bold text-black">
-                {blog.title}
-              </h2>
-              <p className="text-sm text-black mt-2">{blog.excerpt}</p>
-              <div className="mt-3 text-third text-sm">{blog.date}</div>
-              <button className="mt-4 text-fourth font-medium hover:underline">
-                Read More
-              </button>
-            </div>
-            <div className="flex items-center justify-between text-sm text-gray-500">
-              <div className="flex items-center">
-                <div className="w-8 h-8 p-4 m-4 rounded-full bg-gray-300 overflow-hidden">
-                  <Image
-                    src="/public/main.webp" 
-                    alt="Author"
-                    width={40}
-                    height={40}
-                    className="object-cover" />
-                </div>
-                <span className="ml-0">{blog.author}</span>
-              </div>
-            </div>
-           </div>
+  return(
+    <div className="max-w-full mx-auto p-20">
+    <h1 className="text-4xl font-bold text-black mb-10 pt-10 text-center">RECENT BLOGS</h1>
+    <h1 className="text-xl font-semibold text-third mb-20 pb-4 text-center">Here's what we've been to lately </h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14">
+      {blogs.map((blog) => (
+        <div key={blog.id} className="relative shadow-lg rounded-lg hover:shadow-lg hover:scale-105 transition-transform duration-300 hover:bg-gray-200 px-6 m-6 cursor-pointer">
+        <img src={blog.imageUrl} alt={blog.title} className="absolute rounded-full w-28 h-28 -top-12 -left-12 border-4 border-first" />
+          <div className="p-4 max-w-full mt-8">
+           <div className="mt-8 text-black text-sm">{blog.date}</div>
+            <h2 className="mt-2 text-xl font-bold text-black">
+              {blog.title}
+            </h2>
+            <p className="text-md text-second mt-6">{blog.excerpt}</p>
             
-        ))}
-        
-      </div>
-      <div className="w-full flex justify-center mt-12">
-          <Link href="/blogs" className="px-4 py-2 bg-first rounded-lg hover:scale-105 duration-300 hover:duration-500 text-white">
-            More Blogs
-          </Link>
-      </div>
-       
-       
-
+            <button className="mt-8 px-4 py-2 rounded-full shadow-lg bg-first text-white font-medium hover:bg-second hover-scale-105">
+              Read More
+            </button>
+          </div>
+          <div className="flex items-center justify-between text-sm text-black font-semibold p-6">
+            <div className="flex items-center">
+              <div className="w-8 h-8 p-4 m-2 rounded-full bg-gray-300 overflow-hidden">
+                <Image
+                  src="/public/main.webp" 
+                  alt="Author"
+                  width={40}
+                  height={40}
+                  className="object-cover" />
+              </div>
+              <span className="ml-0 font-semibold text-md">{blog.author}</span>
+            </div>
+          </div>
+         </div>
+          
+      ))}
       
     </div>
-    </>
-  );
+    <div className="w-full flex justify-center mt-20">
+        <Link href="/blogs" className="px-6 py-4 bg-first rounded-lg hover:scale-105 duration-300 hover:duration-500 text-white hover:bg-second">
+          More Blogs
+        </Link>
+    </div>
+  </div>
+  )
   
 };
 
