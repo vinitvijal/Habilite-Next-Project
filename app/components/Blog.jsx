f14d22e44b9cbe412ef563b254642ac51e7c8d70
Namiiii-sys <Namitamehra207@gmail.com>
2025-01-02 18:05:05 +0530

    final changes

--- app/components/Blog.jsx
+++ app/components/Blog.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-import Image from "next/image";
 import main from "@/public/main.webp"
+import calendar from "@/public/calendar.svg"
+import scope from "@/public/scope.svg"
+import Image from 'next/image'
 import Link from "next/link";
+
 
 function Blog(){
   const blogs = [
@@ -11,6 +14,7 @@ function Blog(){
       excerpt: "Many state-of-the-art hospitals in Delhi provide advanced treatments...",
       date: "September 30, 2024",
       author: "Dr. Kapil Agrawal",
+      
       imageUrl: "https://www.habiliteclinics.com/assets/images/home/blog-1.webp",
 
     },
@@ -37,13 +41,16 @@ function Blog(){
   return(
     <div className="max-w-full mx-auto p-20">
     <h1 className="text-4xl font-bold text-black mb-10 pt-10 text-center">RECENT BLOGS</h1>
-    <h1 className="text-xl font-semibold text-third mb-20 pb-4 text-center">Here's what we've been to lately </h1>
+    <h1 className="text-2xl font-semibold text-third mb-20 pb-4 text-center">Insights from our Medical Experts</h1>
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14">
       {blogs.map((blog) => (
-        <div key={blog.id} className="relative shadow-lg rounded-lg hover:shadow-lg hover:scale-105 transition-transform duration-300 hover:bg-gray-200 px-6 m-6 cursor-pointer">
-        <img src={blog.imageUrl} alt={blog.title} className="absolute rounded-full w-20 h-20 -top-8 -left-8 border-4 border-first object-cover object-center" />
+        <div key={blog.id} className="relative flex flex-col items-center shadow-lg rounded-lg hover:shadow-lg hover:scale-105 transition-transform duration-300 hover:bg-gray-200 px-6 m-6 cursor-pointer">
+        <img src={blog.imageUrl} alt={blog.title} className="absolute rounded-full w-28 h-28 -top-12 border-4 border-first object-cover object-center" />
           <div className="p-4 max-w-full mt-8">
-           <div className="mt-8 text-black text-sm">{blog.date}</div>
+           <div className="flex justify-start gap-2">
+            <Image src={calendar} className="mt-7 z-0 h-6 w-6 mb-2" alt="none"></Image>
+            <div className="mt-8 text-black text-sm">{blog.date}</div>
+           </div>
             <h2 className="mt-2 text-xl font-bold text-black">
               {blog.title}
             </h2>
@@ -53,20 +60,21 @@ function Blog(){
               Read More
             </button>
           </div>
-          <div className="flex items-center justify-between text-sm text-black font-semibold p-6">
-            <div className="flex items-center">
+          
+            <div className="w-full flex items-center justify-start text-sm text-black font-semibold p-6 gap-2">
               <div className="w-8 h-8 p-4 ml-0 m-2 rounded-full bg-gray-300 overflow-hidden">
                 <Image
                   src="/public/main.webp" 
                   alt="Author"
-                  width={40}
-                  height={40}
+                  width={60}
+                  height={60}
                   className="object-cover" />
               </div>
+              <Image src={scope} className="mt-3 z-0 h-8 w-8 p-1" alt="none"></Image>
               <span className="ml-0 font-semibold text-md">{blog.author}</span>
             </div>
           </div>
-         </div>
+        
           
       ))}
       
