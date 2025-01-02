af481354ac1bab91b487cf1d4d8f0cd77fc16a8a
aparaarachi8145 <aparaaarachi2006@gmail.com>
2025-01-03 00:46:44 +0530

    changes by rishav bhaiyan made

--- app/components/Team.jsx
+++ app/components/Team.jsx
@@ -6,139 +6,135 @@ function Team() {
     <div className="w-screen p-20 "> 
     <div>
        {/* {heading} */}
-     
+       
       <div class="flex flex-col items-center relative pt-20">
        
         <div class="relative flex flex-col items-center mt-5 md:mt-10">
        
-          <h1 class="text-2xl md:text-4xl lg:text-7xl font-semibold text-gray-600">Meet The Team</h1>
-          <div class="md:w-32 w-16 md:h-[4px] h-[2px] bg-first mt-2"></div>
+          <h1 class="text-2xl md:text-4xl lg:text-7xl font-semibold text-first">Meet The Team</h1>
+          <div class="md:w-32 w-16 md:h-[4px] h-[2px] bg-gray-700 mt-2"></div>
         </div>
     </div> <br />
       <p className=' text-gray-500 flex items-center justify-center text-center mx-auto mb-10 text-2xl px-10'> <b>We are a team of qualified psychiatrists and psychologists from premier institutions of the country. <br />We are highly motivated to provide cutting egde evidence based management for mental health issues tailored to individual needs.</b>
       </p>
       </div>
-      <div className='container flex items-center justify-center mx-auto m-10 border-gray-500 border-gray-200 border-t-2 mt-10' >
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-16 w-full max-h-2xl p-10 m-8">
+      <div className='container flex items-center justify-center mx-auto m-10 border-gray-500 border-gray-200 border-t-2 mt-10 mb-10' >
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-16 w-full max-h-xl p-4 m-16">
 
         
-        {/* {card1} */}
-         <div className="bg-white cursor-pointer rounded-lg p-4 border-2 border-gray-50 hover:border-first hover:border-3 transform hover:scale-105 transition-transform duration-300">
-       <div className="flex justify-center item-center transform hover:scale-105 transition-transform duration-300 w-full ">
-       <div className="flex flex-col justify-center bg-white">
-      <div className="group h-96 w-96 [perspective:1000px] p-3">
-        <div className="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
-          <div className="absolute inset-0">
-            <img
-              src = {"/doc1.jpg"}
-              className="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40"
-              alt="SSCBS"/>
-          </div>
-          <div
-            className="absolute inset-0 h-full w-full rounded-xl bg-black/60 px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
-            <div className="flex min-h-full flex-col items-center justify-center text-lg">
-            <p className="text-white font-bold">Hello everyone.</p>
-        <p className="text-white font-bold"> MBBS (MAMC), MD (Gold Medalist) Psychiatry (AIIMS, Delhi), DNB
-        Founder & Head, Habilite Clinic
-        Consultant Psychiatrist and Deaddiction Specialist "</p>
-            </div>
-          </div>
-        </div>
+      {/* {card1} */}
+      <div className="bg-white mt-24 cursor-pointer rounded-lg p-3 border-2 border-gray-50 hover:border-first hover:border-3 transform hover:scale-105 transition-transform duration-300">
+         <div className="mx-auto w-60 h-60 relative -mt-28 shadow-third shadow-lg border-4 border-gray rounded-full overflow-hidden transform hover:scale-105 transition-transform duration-300  ">
+        <img
+          src = {"/doc1.jpg"}
+          className="object-cover object-center h-64"/>
       </div>
-    </div>
-    </div>
       <div className='mx-auto'> 
-<div className="p-4 mt-8 gap-4 text-first text-xl font-bold flex justify-between items-center transform hover:scale-105 transition-transform duration-300">
-  <div className=' border-t-2 border-first w-full'>
-       <button className='inline-flex items-center justify-center mt-6 rounded cursor-pointer w-full bg-blue-600 px-3 py-4 text-2xl font-semibold text-white transition [box-shadow:rgb(171,_196,245)-8px_8px] hover:[box-shadow:rgb(171,_196,_245)0px_0px]'>
-        <div > DR. ADIN AZAM </div>
-        </button> 
-        </div>
+<div className="p-3 mt-2 gap-4 text-first text-xl font-bold flex justify-between items-center transform hover:scale-105 transition-transform duration-300">
+  
+  <div className="flex items-center justify-center mx-auto ">
+
+        <h2 className="font-semibold p-3 text-5xl mt-2 text-second">Sarah Smith</h2>
+      
+    </div>
       </div>
       </div>  
+      <div className="p-3 gap-2 text-xl flex flex-col m-2">
+        <p className="text-second/70 text-center font-bold">MBBS (MAMC), MD (Gold Medalist) Psychiatry (AIIMS, Delhi), DNB
+        Founder & Head, Habilite Clinic Consultant Psychiatrist and Deaddiction Specialist</p>
+        <p className='text-black flex justify-center items-center text-center mt-8'>•10+ years Experience•</p>
+
+        <div className='border-t-2 border-gray-200 mt-3'>
+        <div className='text-gray-400 gap-4 flex justify-center items-center text-center mt-4'>
+        <div className="inline-flex text-2xl">
+                            <svg className="h-6 w-6 text-gray-400 dark:text-gray-600 mr-1" fill="currentColor"
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                <path className=""
+                                    d="M5.64 16.36a9 9 0 1 1 12.72 0l-5.65 5.66a1 1 0 0 1-1.42 0l-5.65-5.66zm11.31-1.41a7 7 0 1 0-9.9 0L12 19.9l4.95-4.95zM12 14a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0-2a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
+                            </svg>Delhi,India • </div>
+      <p className='text-2xl hover:text-first hover:border-b-2 hover:border-first transform hover:scale-105 transition-transform duration-300'>Contact Info</p>
         </div>
-        
+        </div>
+      </div>
+     </div>
+
 
          {/* {card2} */}
-         <div className="bg-white cursor-pointer rounded-lg p-4 border-2 border-gray-50 hover:border-first hover:border-3 transform hover:scale-105 transition-transform duration-300">
-       <div className="flex justify-center item-center transform hover:scale-105 transition-transform duration-300 w-full ">
-       <div className="flex flex-col justify-center bg-white">
-      <div className="group h-96 w-96 [perspective:1000px] p-3">
-        <div className="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
-          <div className="absolute inset-0">
-            <img
-              src = {"/doc2.jpg"}
-              className="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40"
-              alt="SSCBS"/>
-          </div>
-          <div
-            className="absolute inset-0 h-full w-full rounded-xl bg-black/60 px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
-            <div className="flex min-h-full flex-col items-center justify-center text-lg">
-            <p className="text-white font-bold">Hello everyone.</p>
-        <p className="text-white font-bold"> MBBS (MAMC), MD (Gold Medalist) Psychiatry (AIIMS, Delhi), DNB
-        Founder & Head, Habilite Clinic
-        Consultant Psychiatrist and Deaddiction Specialist "</p>
-            </div>
-          </div>
-        </div>
+         <div className="bg-white mt-24 cursor-pointer rounded-lg p-3 border-2 border-gray-50 hover:border-first hover:border-3 transform hover:scale-105 transition-transform duration-300">
+         <div className="mx-auto w-60 h-60 relative -mt-28 shadow-third shadow-lg border-4 border-gray rounded-full overflow-hidden transform hover:scale-105 transition-transform duration-300  ">
+        <img
+          src = {"/doc2.jpg"}
+          className="object-cover object-center h-64"/>
       </div>
-    </div>
-    </div>
       <div className='mx-auto'> 
-<div className="p-4 mt-8 gap-4 text-first text-xl font-bold flex justify-between items-center transform hover:scale-105 transition-transform duration-300">
-  <div className=' border-t-2 border-first w-full'>
-       <button className='inline-flex items-center justify-center mt-6 rounded cursor-pointer w-full bg-blue-600 px-3 py-4 font-semibold text-2xl text-white transition [box-shadow:rgb(171,_196,245)-8px_8px] hover:[box-shadow:rgb(171,_196,_245)0px_0px]'>
-        <div > DR. ADIN AZAM </div>
-        </button> 
-        </div>
+<div className="p-3 mt-2 gap-4 text-first text-xl font-bold flex justify-between items-center transform hover:scale-105 transition-transform duration-300">
+  
+  <div className="flex items-center justify-center mx-auto ">
+
+        <h2 className="font-semibold p-3 text-5xl mt-2 text-second">Sarah Smith</h2>
+      
+    </div>
       </div>
       </div>  
+      <div className="p-3 gap-2 text-xl flex flex-col m-2">
+        <p className="text-second/70 text-center font-bold">MBBS (MAMC), MD (Gold Medalist) Psychiatry (AIIMS, Delhi), DNB
+        Founder & Head, Habilite Clinic Consultant Psychiatrist and Deaddiction Specialist</p>
+        <p className='text-black flex justify-center items-center text-center mt-8'>•10+ years Experience•</p>
+
+        <div className='border-t-2 border-gray-200 mt-3'>
+        <div className='text-gray-400 gap-4 flex justify-center items-center text-center mt-4'>
+        <div className="inline-flex text-2xl">
+                            <svg className="h-6 w-6 text-gray-400 dark:text-gray-600 mr-1" fill="currentColor"
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                <path className=""
+                                    d="M5.64 16.36a9 9 0 1 1 12.72 0l-5.65 5.66a1 1 0 0 1-1.42 0l-5.65-5.66zm11.31-1.41a7 7 0 1 0-9.9 0L12 19.9l4.95-4.95zM12 14a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0-2a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
+                            </svg>Delhi,India • </div>
+      <p className='text-2xl hover:text-first hover:border-b-2 hover:border-first transform hover:scale-105 transition-transform duration-300'>Contact Info</p>
         </div>
+        </div>
+      </div>
+     </div>
+
         
         {/* {card3} */}
-        <div className="bg-white cursor-pointer rounded-lg p-4 border-2 border-gray-50 hover:border-first hover:border-3 transform hover:scale-105 transition-transform duration-300">
-       <div className="flex justify-center item-center transform hover:scale-105 transition-transform duration-300 w-full ">
-       <div className="flex flex-col justify-center bg-white">
-      <div className="group h-96 w-96 [perspective:1000px] p-3">
-        <div className="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
-          <div className="absolute inset-0">
-            <img
-              src = {"/doc3.jpg"}
-              className="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40"
-              alt="SSCBS"/>
-          </div>
-          <div
-            className="absolute inset-0 h-full w-full rounded-xl bg-black/60 px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
-            <div className="flex min-h-full flex-col items-center justify-center text-lg">
-            <p className="text-white font-bold">Hello everyone.</p>
-        <p className="text-white font-bold"> MBBS (MAMC), MD (Gold Medalist) Psychiatry (AIIMS, Delhi), DNB
-        Founder & Head, Habilite Clinic
-        Consultant Psychiatrist and Deaddiction Specialist "</p>
-            </div>
-          </div>
-        </div>
+        <div className="bg-white mt-24 cursor-pointer rounded-lg p-3 border-2 border-gray-50 hover:border-first hover:border-3 transform hover:scale-105 transition-transform duration-300">
+         <div className="mx-auto w-60 h-60 relative -mt-28 shadow-third shadow-lg border-4 border-gray rounded-full overflow-hidden transform hover:scale-105 transition-transform duration-300  ">
+        <img
+          src = {"/doc3.jpg"}
+          className="object-cover object-center h-64"/>
       </div>
-    </div>
-    </div>
       <div className='mx-auto'> 
-<div className="p-4 mt-8 gap-4 text-first text-xl font-bold flex justify-between items-center transform hover:scale-105 transition-transform duration-300">
-  <div className=' border-t-2 border-first w-full'>
-       <button className='inline-flex items-center justify-center mt-6 rounded cursor-pointer w-full bg-blue-600 px-3 py-4 font-semibold 
-       text-2xl text-white transition [box-shadow:rgb(171,_196,245)-8px_8px] hover:[box-shadow:rgb(171,_196,_245)0px_0px]'>
-        <div > DR. ADIN AZAM </div>
-        </button> 
-        </div>
+<div className="p-3 mt-2 gap-4 text-first text-xl font-bold flex justify-between items-center transform hover:scale-105 transition-transform duration-300">
+  
+  <div className="flex items-center justify-center mx-auto ">
+
+        <h2 className="font-semibold p-3 text-5xl mt-2 text-second">Sarah Smith</h2>
+      
+    </div>
       </div>
       </div>  
+      <div className="p-3 gap-2 text-xl flex flex-col m-2">
+        <p className="text-second/70 text-center font-bold">MBBS (MAMC), MD (Gold Medalist) Psychiatry (AIIMS, Delhi), DNB
+        Founder & Head, Habilite Clinic Consultant Psychiatrist and Deaddiction Specialist</p>
+        <p className='text-black flex justify-center items-center text-center mt-8'>•10+ years Experience•</p>
+
+        <div className='border-t-2 border-gray-200 mt-3'>
+        <div className='text-gray-400 gap-4 flex justify-center items-center text-center mt-4'>
+        <div className="inline-flex text-2xl">
+                            <svg className="h-6 w-6 text-gray-400 dark:text-gray-600 mr-1" fill="currentColor"
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                <path className=""
+                                    d="M5.64 16.36a9 9 0 1 1 12.72 0l-5.65 5.66a1 1 0 0 1-1.42 0l-5.65-5.66zm11.31-1.41a7 7 0 1 0-9.9 0L12 19.9l4.95-4.95zM12 14a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0-2a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
+                            </svg>Delhi,India • </div>
+      <p className='text-2xl hover:text-first hover:border-b-2 hover:border-first transform hover:scale-105 transition-transform duration-300'>Contact Info</p>
         </div>
-         {/* {end} */}
-        
+        </div>
+      </div>
+     </div>
       </div>
       </div>
     </div>
   )
 }
 
-
 export default Team
-
