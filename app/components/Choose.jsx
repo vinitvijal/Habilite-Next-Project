7ea890231b962d2be69a5e40b502ba67329ea35b
tomatocode <adinazam70076@gmail.com>
2025-01-03 12:51:44 +0530

    section redesigned

--- app/components/Choose.jsx
+++ app/components/Choose.jsx
@@ -3,120 +3,115 @@ import Image from "next/image";
 function Choose() {
   return (
     <section id="Choose" className="py-20 w-screen">
-
-
-
       <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
-      <div class="relative flex flex-col items-center mt-5 md:mt-5">
-       
-       <h1 class="text-2xl md:text-4xl lg:text-6xl font-semibold text-gray-600">Why Choose Us?</h1>
-       <div class="md:w-32 w-16 md:h-[4px] h-[2px] bg-first mt-2"></div>
-     </div>
-
-        
+        <h2 className="text-first text-center font-bold uppercase tracking-wide text-3xl m-8 p-8">
+          Why Choose Us?
+        </h2>
       </div>
-      <div className="container flex items-center justify-center mx-auto mt-10 ">
-      <div className="inline-flex">
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-16 w-full max-h-2xl p-10 m-4">
 
-     {/* {#card-1} */}
-     <div className="overflow-hidden py-4 sm:py-8">
-    <div
-        className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
-        <span className="absolute top-10 z-0 h-20 w-20 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[10]"></span>
-        <div className="relative z-10 mx-auto max-w-md">
-            <span className="grid h-20 w-20 place-items-center rounded-full bg-sky-500 transition-all duration-300 group-hover:bg-sky-400">
-                <svg xmlns="./things.svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-10 w-10 text-white transition-all">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
-                </svg>
-            </span>
-            <div
-                class="space-y-6 pt-5 transition-all duration-300 group-hover:text-white/90">
-                  <h1 className="font-bold text-xl">◉ Your health is our priority</h1>
-                <p className="text-base text-gray-600 font-b ">Our clinic is home to a team of highly qualified doctors,
+      {/* {#card-1} */}
+
+      <div className=" flex items-center justify-center mx-auto m-10 mt-10 ">
+        <div className="grid grid-cols-4 md:grid-cols-4 gap-4 ">
+          <div className="shadow-sm p-6 text-center group w-64 h-96 bg-white border:gray-700 border-4 hover:border-white relative m-4 hover:ring hover:ring-first   transform duration-500 hover:scale-105 hover:shadow-xl">
+            <div className="bg-first flex justify-center items-center w-20 h-20 rounded-full absolute mx-auto right-0 left-0 -inset-y-14 border-4 group-hover:bg-white group-hover:shadow-md transform duration-300">
+              <span className="text-3xl md:text-5xl text-white  group-hover:text-first form duration-300 p-4">
+                <img
+                  width="48"
+                  height="48"
+                  src="https://img.icons8.com/color/48/heart-with-pulse.png"
+                  alt="heart-with-pulse"
+                />
+              </span>
+            </div>
+            <div className="flex flex-col items-center justify-center absolute mx-auto inset-0 p-4">
+              <h3 className="text-xl font-bold mb-2">
+                Your health is our priority.
+              </h3>
+              <p className="text-third-100">
+                Our clinic is home to a team of highly qualified doctors,
                 specialists, and healthcare staff with decades of combined
                 experience. We ensure every patient receives expert guidance and
-                treatment tailored to their unique needs.</p>
+                treatment tailored to their unique needs.
+              </p>
             </div>
-        </div>
-    </div>
-</div>
+          </div>
 
-           {/* {#card-2} */}
+          {/* Card 2 */}
 
-           <div className="overflow-hidden py-4 sm:py-8">
-    <div
-        className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
-        <span className="absolute top-10 z-0 h-20 w-20 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[10]"></span>
-        <div className="relative z-10 mx-auto max-w-md">
-            <span className="grid h-20 w-20 place-items-center rounded-full bg-sky-500 transition-all duration-300 group-hover:bg-sky-400">
-                <svg xmlns="./things.svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-10 w-10 text-white transition-all">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
-                </svg>
-            </span>
-            <div
-                class="space-y-6 pt-5 transition-all duration-300 group-hover:text-white/90">
-                  <h1 className="font-bold text-xl">◉  Experienced Medical Professionals</h1>
-                <p className="text-base text-gray-600 font-b ">Our clinic is home to a team of highly qualified doctors,
+          <div className=" shadow-sm p-6 text-center group w-64 h-96 bg-white border:gray-700 border-4 hover:border-white relative m-4 hover:ring hover:ring-first   transform duration-500 hover:scale-105 hover:shadow-xl">
+            <div className="bg-first  flex justify-center items-center w-20 h-20 rounded-full absolute mx-auto right-0 left-0 -inset-y-14 border-4 group-hover:bg-white group-hover:shadow-md transform duration-300">
+              <span className="text-3xl md:text-5xl text-white  group-hover:text-first  transform duration-300 p-4">
+                <img
+                  width="48"
+                  height="48"
+                  src="https://img.icons8.com/fluency/48/nurse-female.png"
+                  alt="nurse-female"
+                />
+              </span>
+            </div>
+
+            <div className="flex flex-col items-center justify-center absolute mx-auto inset-0 p-4">
+              <h3 className="text-xl font-bold mb-2">
+                Experienced Medical Professionals
+              </h3>
+              <p className="text-third-100">
+                Our clinic is home to a team of highly qualified doctors,
                 specialists, and healthcare staff with decades of combined
                 experience. We ensure every patient receives expert guidance and
-                treatment tailored to their unique needs.</p>
+                treatment tailored to their unique needs.
+              </p>
             </div>
-        </div>
-    </div>
-</div>
+          </div>
 
-{/* {#card-3} */}
+          {/* {#card-3} */}
 
-<div className="overflow-hidden py-4 sm:py-8">
-    <div
-        className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
-        <span className="absolute top-10 z-0 h-20 w-20 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[10]"></span>
-        <div className="relative z-10 mx-auto max-w-md">
-            <span className="grid h-20 w-20 place-items-center rounded-full bg-sky-500 transition-all duration-300 group-hover:bg-sky-400">
-                <svg xmlns="./things.svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-10 w-10 text-white transition-all">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
-                </svg>
-            </span>
-            <div
-                class="space-y-6 pt-5 transition-all duration-300 group-hover:text-white/90">
-                  <h1 className="font-bold text-xl">◉ Advanced Facilities & Technology</h1>
-                <p className="text-base text-gray-600 font-b "> Our clinic is home to a team of highly qualified doctors,
-                specialists, and healthcare staff with decades of combined
-                experience. We ensure every patient receives expert guidance and
-                treatment tailored to their unique needs.</p>
+          <div className=" shadow-sm p-6 text-center group w-64 h-96 bg-white border:gray-700 border-4 hover:border-white  relative m-4 hover:ring hover:ring-first   transform duration-500 hover:scale-105 hover:shadow-xl">
+            <div className="bg-first  flex justify-center items-center w-20 h-20 rounded-full absolute mx-auto right-0 left-0 -inset-y-14 border-4 group-hover:bg-white group-hover:shadow-md transform duration-300">
+              <span className="text-3xl md:text-5xl text-white  group-hover:text-first form duration-300 p-4">
+                <img
+                  width="64"
+                  height="64"
+                  src="https://img.icons8.com/?size=100&id=H0fT5t4BS8rR&format=png&color=000000"
+                  alt="external-medical-equipment-healthcare-and-medicine-filled-outline-perfect-kalash"
+                />
+              </span>
             </div>
-        </div>
-    </div>
-</div>
+            <div className="flex flex-col items-center justify-center absolute mx-auto inset-0 p-4">
+              <h3 className="text-xl font-bold mb-2">
+                Advanced Facilities & Technology
+              </h3>
+              <p className="text-third-100">
+                We pride ourselves on utilizing state-of-the-art medical
+                equipment and modern facilities to provide accurate diagnoses
+                and effective treatments. From routine checkups to specialized
+                procedures, we deliver top-notch care.
+              </p>
+            </div>
+          </div>
 
           {/* {#card-4} */}
 
-          <div className="overflow-hidden py-4 sm:py-8">
-    <div
-        className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
-        <span className="absolute top-10 z-0 h-20 w-20 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[10]"></span>
-        <div className="relative z-10 mx-auto max-w-md">
-            <span className="grid h-20 w-20 place-items-center rounded-full bg-sky-500 transition-all duration-300 group-hover:bg-sky-400">
-                <svg xmlns="./things.svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-10 w-10 text-white transition-all">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
-                </svg>
-            </span>
-            <div
-                class="space-y-6 pt-5 transition-all duration-300 group-hover:text-white/90">
-                  <h1 className="font-bold text-xl">◉ Patient-Centered Approach</h1>
-                <p className="text-base text-gray-600 font-b ">Our clinic is home to a team of highly qualified doctors,
-                specialists, and healthcare staff with decades of combined
-                experience. We ensure every patient receives expert guidance and
-                treatment tailored to their unique needs.</p>
+          <div className=" shadow-sm p-6 text-center group w-64 h-96 bg-white border:gray-700 border-4 hover:border-white  relative m-4 hover:ring hover:ring-first   transform duration-500 hover:scale-105 hover:shadow-xl">
+            <div className="bg-first  flex justify-center items-center w-20 h-20 rounded-full absolute mx-auto right-0 left-0 -inset-y-14 border-4 group-hover:bg-white group-hover:shadow-md transform duration-300">
+            <span className="text-3xl md:text-5xl text-white  group-hover:text-first form duration-300 p-4">
+            <img width="66" height="66" src="https://img.icons8.com/external-smashingstocks-flat-smashing-stocks/66/external-Doctor-And-Patient-medical-concepts-smashingstocks-flat-smashing-stocks-4.png" alt="external-Doctor-And-Patient-medical-concepts-smashingstocks-flat-smashing-stocks-4"/>
+              </span>
             </div>
+            <div className="flex flex-col items-center justify-center absolute mx-auto inset-0 p-4">
+              <h3 className="text-xl font-bold mb-2">
+                Patient-Centered Approach.
+              </h3>
+              <p className="text-third-100">
+                {" "}
+                We pride ourselves on utilizing state-of-the-art medical
+                equipment and modern facilities to provide accurate diagnoses
+                and effective treatments. From routine checkups to specialized
+                procedures, we deliver top-notch care.
+              </p>
+            </div>
+          </div>
         </div>
-    </div>
-</div>
-          
-        
-        </div>
-      </div>
       </div>
     </section>
   );
