515e89f19dc6300cfc75523ecd41522b30fe34d8
roshni <roshni.23545@sscbs.du.ac.in>
2025-01-06 00:36:05 +0530

    completed medium screen footer design

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,67 +1,72 @@
 import React from 'react'
+import { FaSquareXTwitter, FaSquareYoutube, FaSquareFacebook } from "react-icons/fa6";
+import { FaPinterestSquare, FaLinkedin } from "react-icons/fa";
+
 
 function Footer() {
   return (
       <section id='footer' className='bg-blue-700 flex h-full flex-col '> 
         {/* <div className='flex justify-center items-center bg-white rounded-b-full h-[5vh] w-full'></div> */}
         <img src='/foot-design.png' className='w-full'/>
-        <div className='flex justify-center items-center gap-20 px-52 py-10'>
-        <div className='flex-col justify-center items-center'>
-            <h1 className='font-bold text-2xl text-white'>Habilite Clinic.</h1>
+        <div className='flex justify-center items-start px-28 py-12 gap-12'>
+        <div className='flex-col justify-center items-center w-1/4'>
+            <h1 className='font-bold text-3xl text-white'>Habilite Clinic.</h1>
             <h1 className='font-semibold text-md text-white/50'>Weight Loss | Laprosy | Laser</h1>
             <div className='flex items-center gap-4 py-5'>
-            <img src="/social-media-icons/linkedIn.svg" className='h-6 w-6'/>
-            <img src="/social-media-icons/facebook.png" className='h-6 w-6'/>
-            <img src="/social-media-icons/Youtube.png" className='h-6 w-6'/>
-            <img src="/social-media-icons/pinterest.svg" className='h-7 w-7'/>
-            <img src="/social-media-icons/twitter.svg" className='h-7 w-7'/>
+            <FaLinkedin className='h-7 w-7 cursor-pointer' color='#d0def5'/>
+            <FaSquareFacebook className='h-7 w-7 cursor-pointer' color='#d0def5'/>
+            <FaSquareYoutube className='h-7 w-7 cursor-pointer' color='#d0def5'/>
+            <FaPinterestSquare className='h-7 w-7 cursor-pointer' color='#d0def5'/>
+            <FaSquareXTwitter className='h-7 w-7 cursor-pointer' color='#d0def5'/>
             </div>
             <div className='flex flex-col pt-6 gap-4'>
-            <div className='flex items-center gap-2'>
-              <img src="/call.png" className='h-5 w-5'/>
-              <h1 className='text-md text-white/80 text-light'>+(91) 9999456455</h1>
+            <div className='flex items-center gap-2 cursor-pointer group'>
+              <img src="/call.png" className='h-5 w-5 transform transition-all duration-500 ease-in-out group-hover:-translate-y-1 group-hover:w-6 group-hover:h-6'/>
+              <h1 className='text-sm text-white/80 text-light '>+(91) 9999456455</h1>
             </div>
-            <div className='flex items-center gap-2'>
-              <img src="/call.png" className='h-5 w-5'/>
-              <h1 className='text-md text-white/80 text-light'>+(91) 9910024564</h1>
+            <div className='flex items-center gap-2 cursor-pointer group'>
+              <img src="/call.png" className='h-5 w-5 transform transition-all duration-500 ease-in-out group-hover:-translate-y-1 group-hover:w-6 group-hover:h-6'/>
+              <h1 className='text-sm text-white/80 text-light'>+(91) 9910024564</h1>
             </div>
-            <div className='flex items-center gap-2'>
-              <img src="/mail.png" className='h-5 w-5'/>
-              <h1 className='text-md text-white/80 text-light '>contact@habiliteclinics.com</h1>
+            <div className='flex items-center gap-2 cursor-pointer group'>
+              <img src="/mail.png" className='h-5 w-5 transform transition-all duration-500 ease-in-out group-hover:-translate-y-1 group-hover:w-6 group-hover:h-6'/>
+              <h1 className='text-sm text-white/80 text-light'>contact@habiliteclinics.com</h1>
             </div>
-            <div className='flex items-center gap-2'>
-              <img src="/location.png" className='h-5 w-5'/>
-              <h1 className='text-md text-white/80 text-light'>M 11, Block M, Lajpat Nagar Road,Lajpat Nagar 2, Lajpat Nagar, New Delhi, Delhi 110024</h1>
+            <div className='flex items-center gap-2 cursor-pointer group'>
+              <img src="/location.png" className='h-5 w-5 transform transition-all duration-500 ease-in-out group-hover:-translate-y-1 group-hover:w-6 group-hover:h-6'/>
+              <h1 className='text-sm text-white/80 text-light '>M 11, Block M, Lajpat Nagar Road,Lajpat Nagar 2, Lajpat Nagar, New Delhi, Delhi 110024</h1>
             </div>
-            <div className='flex items-center gap-2'>
-              <img src="/location.png" className='h-5 w-5'/>
-              <h1 className='text-md text-white/80 text-light'>C-7/186, SDA, Hauz Khas, New Delhi, Delhi 110016</h1>
+            <div className='flex items-center gap-2 cursor-pointer group'>
+              <img src="/location.png" className='h-5 w-5 transform transition-all duration-500 ease-in-out group-hover:-translate-y-1 group-hover:w-6 group-hover:h-6'/>
+              <h1 className='text-sm text-white/80 text-light '>C-7/186, SDA, Hauz Khas, New Delhi, Delhi 110016</h1>
             </div>
             </div>
         </div>
+        <div className='flex items-start justify-center gap-10 grid-cols-3 py-10'>
         <div className='flex-col items-center text-left'>
-          <h1 className='text-xl text-white/60 text-extrabold pb-8'>Laparoscopic Surgery</h1>
-          <p className='text-md text-white/80 text-light py-2'>Gall Stones Treatment</p>
-          <p className='text-md text-white/80 text-light py-2'>Laparoscopic Hernia Treatment</p>
-          <p className='text-md text-white/80 text-light py-2'>Rectal prolapse</p>
-          <p className='text-md text-white/80 text-light py-2'>Appendix</p>
-          <p className='text-md text-white/80 text-light py-2'>Gerd</p>
+          <h1 className='text-xl text-white/90 text-semibold pb-8'>Laparoscopic Surgery</h1>
+          <a><p className='text-md text-white/70 text-light py-2 cursor-pointer hover:text-white'>Gall Stones Treatment</p></a>
+          <a><p className='text-md text-white/70 text-light py-2 cursor-pointer hover:text-white'>Laparoscopic Hernia Treatment</p></a>
+          <a><p className='text-md text-white/70 text-light py-2 cursor-pointer hover:text-white'>Rectal prolapse</p></a>
+          <a><p className='text-md text-white/70 text-light py-2 cursor-pointer hover:text-white'>Appendix</p></a>
+          <a><p className='text-md text-white/70 text-light py-2 cursor-pointer hover:text-white'>Gerd</p></a>
         </div>
         <div className='flex-col items-center text-left'>
-          <h1 className='text-xl text-white/60 text-extrabold pb-8'>Laparoscopic Surgery</h1>
-          <p className='text-md text-white/80 text-light py-2'>Gall Stones Treatment</p>
-          <p className='text-md text-white/80 text-light py-2'>Laparoscopic Hernia Treatment</p>
-          <p className='text-md text-white/80 text-light py-2'>Rectal prolapse</p>
-          <p className='text-md text-white/80 text-light py-2'>Appendix</p>
-          <p className='text-md text-white/80 text-light py-2'>Gerd</p>
+        <h1 className='text-xl text-white/90 text-semibold pb-8'>Bariatric Surgery</h1>
+        <a><p className='text-md text-white/70 text-light py-2 cursor-pointer hover:text-white'>Best Bariatric surgeon in Delhi</p></a>
+          <a><p className='text-md text-white/70 text-light py-2 cursor-pointer hover:text-white'>Non-surgical weight loss program</p></a>
+          <a><p className='text-md text-white/70 text-light py-2 cursor-pointer hover:text-white'>Surgery for diabetes</p></a>
+          <a><p className='text-md text-white/70 text-light py-2 cursor-pointer hover:text-white'>Intragastric Balloon For Weight Loss</p></a>
         </div>
         <div className='flex-col items-center text-left'>
-          <h1 className='text-xl text-white/60 text-extrabold pb-8'>Laparoscopic Surgery</h1>
-          <p className='text-md text-white/80 text-light py-2'>Gall Stones Treatment</p>
-          <p className='text-md text-white/80 text-light py-2'>Laparoscopic Hernia Treatment</p>
-          <p className='text-md text-white/80 text-light py-2'>Rectal prolapse</p>
-          <p className='text-md text-white/80 text-light py-2'>Appendix</p>
-          <p className='text-md text-white/80 text-light py-2'>Gerd</p>
+        <h1 className='text-xl text-white/90 text-semibold pb-8'>Laser Surgeryy</h1>
+        <a><p className='text-md text-white/70 text-light py-2 hover:text-white'>Hemorrhoids / Piles Treatment</p></a>
+          <a><p className='text-md text-white/70 text-light py-2 cursor-pointer hover:text-white'>Anal fistula Treatment</p></a>
+          <a><p className='text-md text-white/70 text-light py-2 cursor-pointer hover:text-white'>Pilonidal Sinus</p></a>
+          <a><p className='text-md text-white/70 text-light py-2 cursor-pointer hover:text-white'>Anal fissure Treatment</p></a>
+          <a><p className='text-md text-white/70 text-light py-2 cursor-pointer hover:text-white'>Lipoma</p></a>
+          <a><p className='text-md text-white/70 text-light py-2 cursor-pointer hover:text-white'>Circumcision</p></a>
+        </div>
         </div>
         </div>
         <div className='bg-blue-800 justify-center items-center flex'>
