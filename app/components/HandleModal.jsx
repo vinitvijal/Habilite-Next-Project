d0e3321cef09a1d7427ad72f9210650281cb0a62
Pancreass <mragankmuni000@gmail.com>
2025-01-05 18:45:40 +0530

    icons added in form

--- app/components/HandleModal.jsx
+++ app/components/HandleModal.jsx
@@ -10,7 +10,7 @@ function HandleModal() {
 
   return (
     <>
-    {!showModal?<button className='group fixed z-20 max-sm:hover:scale-110 sm:bottom-10 bottom-6 right-6 sm:right-10 bg-first flex items-center rounded-lg p-2 text-white sm:text-xl font-semibold shadow-md shadow-black sm:shadow-lg sm:shadow-black' onClick={()=>{setShowModal(true);setShowButton(false)}} >
+    {!showModal?<button className='group fixed z-20 max-sm:hover:scale-110 sm:bottom-10 bottom-6 right-6 sm:right-10 bg-first flex items-center rounded-lg p-2 text-white sm:text-xl font-semibold shadow-md shadow-black sm:shadow-lg sm:shadow-black' onClick={()=>{setShowModal(true)}} >
       <FaBook className='text-white max-sm:hidden size-8 sm:my-1 mx-2' />
       <div className=' sm:overflow-hidden sm:transition-all  sm:duration-500 sm:ease-in-out w-auto sm:max-w-0 group-hover:sm:max-w-xs group-hover:duration-500 sm:inline-block' ><span className="flex items-center  w-full whitespace-nowrap">Book Appointment <FaArrowRightLong className='size-4 text-white ml-1 pt-1 ' /> </span></div>
     </button>:null}
--- app/components/Modal.jsx
+++ app/components/Modal.jsx
@@ -32,20 +32,32 @@ function Modal({isVisible, onClose}) {
   return (
     <div id='appointment' className='fixed z-50 inset-0 bg-opacity-70 bg-black backdrop-blur-sm  w-full flex items-center justify-center' >
       <div className={`mx-12  max-sm:min-w-[60vw] sm:flex  rounded-md ${showAnimation ? 'animate-slideindown' : 'animate-slideoutup'} `}>
-        <form className=' bg-white h-fit items-center justify-center shadow-black shadow-sm rounded-md ' >
-          <div className="px-5 pt-3 pb-1 font-semibol text-xl sm:text-3xl text-center border-b border-third rounded-tl-md rounded-tr-md ">Book An Appointment</div>
-          <div className="px-5 sm:px-8 py-3 pb-2 flex flex-col rounded-bl-md rounded-br-md ">
+        <form className='px-5 bg-white h-fit items-center justify-center shadow-black shadow-sm rounded-md ' >
+          <div className=" pt-3 pb-1 font-semibol text-xl sm:text-3xl text-center border-b border-third rounded-tl-md rounded-tr-md ">Book An Appointment</div>
+          <div className="sm:px-5  py-3 pb-2 flex flex-col rounded-bl-md rounded-br-md ">
 
-            <label htmlFor="firstName" className='mt-3 font-semibold' >Full Name</label>
+            <div className="flex items-center justify-start">
+              <IoPersonSharp className='size-5 -mb-2 mx-1 ' />
+              <label htmlFor="firstName" className='mt-3 ' >Full Name</label>
+            </div>
             <input type="text" name="fullName" id="fullName" className=' rounded-md py-1 px-2 border border-third placeholder:text-sm 'placeholder='Enter Your Full Name' required/>
 
-            <label htmlFor="phoneNum" className='mt-3 font-semibold'>Phone Number</label>
+            <div className="flex items-center justify-start">
+              <MdCall className='size-5 -mb-2 mx-1 ' />
+              <label htmlFor="phoneNum" className='mt-3 '>Phone Number</label>
+            </div>
             <input type="text" name="phoneNum" id="phoneNum" className=' rounded-md py-1 px-2 border border-third placeholder:text-sm' placeholder='Enter Your Phone Number' required />
             
-            <label htmlFor="email" className='mt-3 font-semibold'>Email ID</label>
+            <div className="flex items-center justify-start">
+              <IoMail className='size-5 -mb-3 mx-1 ' />
+              <label htmlFor="email" className='mt-3 '>Email ID</label>
+            </div>
             <input type="email" name="email" id="email" className=' rounded-md py-1 px-2 border border-third placeholder:text-sm' placeholder='Enter Your Email Address' required />
             
-            <label htmlFor="query" className='mt-3 font-semibold'>Query</label>
+            <div className="flex items-center justify-start">
+              <FaPen className='size-5 -mb-2 mx-1 ' />
+              <label htmlFor="query" className='mt-3 '>Query</label>
+            </div>
             <textarea name='query' id='query' placeholder='Write Your Query' rows={4} className='rounded-md py-1 px-2 border border-third placeholder:text-sm' required />
             <div className="flex items-center space-x-4 py-3">
               <button type="submit" className=' bg-first w-full py-1 hover:bg-second border-2 border-first hover:border-second focus:scale-90 text-white font-semibold rounded-md' onClick={handleSubmit}>Submit</button>
