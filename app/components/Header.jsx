cb2bb2f805149be09b7091347a66e757679b6d6e
aparaarachi8145 <aparaaarachi2006@gmail.com>
2025-01-06 21:10:10 +0530

    layout changed

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -3,25 +3,26 @@ import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 
 function Header() {
   return (
-    <header className=' flex px-48 justify-between items-center w-full h-24 border-b'>
-      <div className=''>
-        <h1 className=' text-2xl font-bold text-first'>Habilite Clinic.</h1>
-      </div>
-      <div>
-        <nav>
-          <ul className=' flex justify-center items-center gap-20 font-medium text-lg  '>
-            <li>Home</li>
-            <li>About</li>
-            <li>Surgery</li>
-            <li>Blog</li>
-            <li>Contact Us</li>
-          </ul>
-        </nav>
-      </div>
-      <div>
-        <button className=' px-3 py-2 rounded-md bg-first text-white'>Book Appointment</button>
-      </div>
-    </header>
+    // <header classNameName=' flex px-48 justify-between items-center w-full h-24 border-b'>
+    //   <div classNameName=''>
+    //     <h1 classNameName=' text-2xl font-bold text-first'>Habilite Clinic.</h1>
+    //   </div>
+    //   <div>
+    //     <nav>
+    //       {/* <ul classNameName=' flex justify-center items-center gap-20 font-medium text-lg  '>
+    //         <li>Home</li>
+    //         <li>About</li>
+    //         <li>Surgery</li>
+    //         <li>Blog</li>
+    //         <li>Contact Us</li>
+    //       </ul> */}
+    //     </nav>
+    //   </div>
+    //   <div>
+    //     <button classNameName=' px-3 py-2 rounded-md bg-first text-white'>Book Appointment</button>
+    //   </div>
+    // </header>
+    <div></div>
   )
 }
 
