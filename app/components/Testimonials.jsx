dc9c7ea9f1d2fae19ff91bb867275a5923ce5849
Harshit <harshitkumar100436@gmail.com>
2025-01-06 20:22:42 +0530

    Design changes(6-01-25)

--- app/components/Testimonials.jsx
+++ app/components/Testimonials.jsx
@@ -3,45 +3,33 @@ import Marquee from "/components/ui/marquee";
 
 const reviews = [
   {
-    name: "Frank Castle",
-    occupation: "The Punisher1",
-    review: "After the death of Madhavrao Peshwa in 1772, his brother Narayanrao became peshwa (prime minister) of the Maratha Empire. His palace guards murdered Narayanrao in August 1773, and his uncle Raghunathrao (Raghoba) became Peshwa..",
-    
+    name: "Dilip Kumar",
+    occupation: "Businessman",
+    review: "My weight was  123 kgs and life was very difficult for me. After exploring all the weight-loss options with no significant success, I decided to explore the possibility of bariatric surgery. I started looking for the best bariatric surgeon in Delhi NCR and finally met Dr. Kapil Agrawal. I underwent bariatric surgery, and after 11 months, I was 65 kg. i am ver happy with my decision. Many thanks to Dr. Kapil Agrawal.",
+
   },
   {
-    name: "Frank Castle",
-    occupation: "The Punisher2",
-    review: "After the death of Madhavrao Peshwa in 1772, his brother Narayanrao became peshwa (prime minister) of the Maratha Empire. His palace guards murdered Narayanrao in August 1773, and his uncle Raghunathrao (Raghoba) became Peshwa..",
-    
+    name: "T.N. Kambara",
+    occupation: "Businessman",
+    review: "Dr. Kapil Agrawal and his team performed a sleeve gastrectomy about five months back. I had three minor cuts on my abdomen. I was discharged on the next day of surgery. The speed and extent of recovery at home have amazed my friends and family. I had lost about 24 kg of weight in 4 months, and the weight loss is still going on. My back feels much better, and now I can walk at least 3 miles at a stretch.",
+
   },
   {
-    name: "Frank Castle",
-    occupation: "The Punisher3",
-    review: "After the death of Madhavrao Peshwa in 1772, his brother Narayanrao became peshwa (prime minister) of the Maratha Empire. His palace guards murdered Narayanrao in August 1773, and his uncle Raghunathrao (Raghoba) became Peshwa...",
-    
+    name: "SONU GANDHI",
+    occupation: "Teacher",
+    review: "I want to thank Dr. Kapil Agrawal and his team for the excellent results.  I underwent as a weight loss surgery  as i weigh 115 kg. He helped me in making the big decision. After eight months of surgery, I am so happy and relieved because my weight came down to 65 kg, and I feel very  energetic. I want to thank him for offering me a new lease on life. God bless him. Undoubtedly, one of the finest and best bariatric surgeons.",
+
   },
   {
-    name: "Frank Castle",
-    occupation: "The Punisher4",
-    review: "After the death of Madhavrao Peshwa in 1772, his brother Narayanrao became peshwa (prime minister) of the Maratha Empire. His palace guards murdered Narayanrao in August 1773, and his uncle Raghunathrao (Raghoba) became Peshwa...",
-    
-  },
-  {
-    name: "Frank Castle",
-    occupation: "The Punisher5",
-    review: "After the death of Madhavrao Peshwa in 1772, his brother Narayanrao became peshwa (prime minister) of the Maratha Empire. His palace guards murdered Narayanrao in August 1773, and his uncle Raghunathrao (Raghoba) became Peshwa..",
-    
-  },
-  {
-    name: "Frank Castle",
-    occupation: "The Punisher6",
-    review: "After the death of Madhavrao Peshwa in 1772, his brother Narayanrao became peshwa (prime minister) of the Maratha Empire. His palace guards murdered Narayanrao in August 1773, and his uncle Raghunathrao (Raghoba) became Peshwa..",
-    
+    name: "Rajini",
+    occupation: "Engineer",
+    review: "I want to thank Dr. Kapil Agrawal and his team. My weight was 145 kg. Despite my best efforts, my weight kept increasing. I started looking for the best weight loss surgeon in Delhi, and after meeting some of the bariatric surgeons, my search finally got over after meeting Dr. Kapil Agrawal. He performed mini-gastric bypass on me, and after eight months, my weight is 85 kg. He is very kind and professional surgeon.",
+
   },
 ];
 
-const firstRow = reviews.slice(0, reviews.length / 2);
-const secondRow = reviews.slice(reviews.length / 2);
+
+const firstRow = reviews.slice(0, reviews.length);
 
 const ReviewCard = ({
   name,
@@ -51,18 +39,18 @@ const ReviewCard = ({
   return (
     <figure
       className={cn(
-        "relative w-64 cursor-pointer overflow-hidden p-4 border-b-2 border-r-2 rounder-bl-xl rounded-br-xl shadow-md shadow-blue-100",
+        "relative w-[200px] sm:w-80 cursor-pointer overflow-hidden p-3 sm:p-4 border-first border-2 rounded-lg",
       )}
-    >
-      <div className="flex flex-row items-center">
+    > 
+      <blockquote className="text-xs sm:text-sm pb-1">{review}</blockquote>
+      <div className="mt-3 flex flex-row items-center">
         <div className="flex flex-col">
-          <figcaption className="text-xl font-medium dark:text-white">
+          <figcaption className="text-lg sm:text-xl font-semibold dark:text-white">
             {name}
           </figcaption>
-          <p className="text-lg font-medium dark:text-white/40">{occupation}</p>
+          <span className="text-sm sm:text-base font-medium dark:text-white/40">{occupation}</span>
         </div>
       </div>
-      <blockquote className="mt-2 text-sm">{review}</blockquote>
     </figure>
   );
 };
@@ -70,22 +58,17 @@ const ReviewCard = ({
 function Testimonials() {
   return (
     <>
-    <h1 className="text-3xl font-bold text-first flex justify-center item-center">Our Testimonials</h1>
-    <div className="relative flex h-[700px] w-full flex-col items-center justify-center overflow-hidden bg-background">
-          <Marquee pauseOnHover className="[--duration:20s]">
-            {firstRow.map((review) => (
-              <ReviewCard key={review.occupation} {...review} />
-            ))}
-          </Marquee>
-          <Marquee reverse pauseOnHover className="[--duration:20s]">
-            {secondRow.map((review) => (
-              <ReviewCard key={review.occupation} {...review} />
-            ))}
-          </Marquee>
-          <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
-          <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
-        </div>
-        </>
+      <div className="relative flex flex-col items-center justify-center h-[450px] sm:h-[500px] overflow-hidden bg-background">
+        <h1 className="text-2xl sm:text-3xl font-bold text-first flex justify-center items-center mb-6">Our Testimonials</h1>
+        <Marquee pauseOnHover className="w-full [--duration:20s] mb-4">
+          {firstRow.map((review) => (
+            <ReviewCard key={review.name} {...review} />
+          ))}
+        </Marquee>
+        <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
+        <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
+      </div>
+    </>
   );
 }
 export default Testimonials;
