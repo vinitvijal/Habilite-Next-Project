0f92d68506ee8179dbb336ace2e4a477fd77c893
soubhagyacodes <soubhagya613mishra@gmail.com>
2025-01-07 23:43:03 +0530

    Header Revamp 5

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -17,33 +17,30 @@ function Header() {
             </label>
 
             <label htmlFor="about" className='hover:cursor-pointer group'>
-              <input type="radio" className='sr-only peer' id='about' name='navigator'/>
-                    <p className="select-none relative flex items-center gap-1 rounded-3xl px-5 py-2 text-gray-500 hover:text-black transition-all duration-300 ease-in-out group-hover:text-first">About <svg className="group-hover:rotate-180 group-hover:translate-y-[2px]" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path className="" d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg></p>
+              <input type="radio" className='sr-only peer' id='about' name='dropdown'/>
+                    <p className="select-none h-20  relative flex items-center gap-1 rounded-3xl px-5 py-2 text-gray-500 hover:text-black transition-all duration-300 ease-in-out group-hover:text-first">About <svg className="group-hover:rotate-180 group-hover:translate-y-[2px]" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path className="" d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg></p>
                             
-                            <div className="DropdownMenuContent group hidden group-hover:block absolute bg-white w-40 z-50 translate-x-2 translate-y-[0.7px] shadow-xl rounded-md">
+                            <div className="DropdownMenuContent group hidden group-hover:block absolute bg-white w-48 z-50 translate-x-2 -translate-y-2 shadow-xl rounded-md">
 
-                                <p className="DropdownMenuItem h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                                    Test 1
+                                <p className="DropdownMenuItem text-md h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
+                                    About Habilite Clinic
                                 </p>
 
-                                <p className="DropdownMenuItem h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                                    Test 2 
+                                <p className="DropdownMenuItem text-md h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
+                                    Dr. Kapil Agarwal 
                                 </p>
 
-                                <p className="DropdownMenuItem h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                                    Test 3 
-                                </p>
 
                             </div>
               {/* <p className="select-none rounded-3xl px-5 py-2 text-gray-500 hover:text-black peer-checked:text-white peer-checked:bg-first transition-all duration-300 ease-in-out">About</p>  */}
             </label>
 
             <label htmlFor="surgery" className='hover:cursor-pointer group'>
-              <input type="radio" className='sr-only peer' id='surgery' name='navigator'/>
-                    <p className="select-none relative flex items-center gap-1 rounded-3xl px-5 py-2 text-gray-500 hover:text-black transition-all duration-300 ease-in-out group-hover:text-first">Surgery <svg className="group-hover:rotate-180 group-hover:translate-y-[2px]" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg></p>
-                        <div className="DropdownMenuContent hover:block hidden group-hover:block absolute bg-white w-40 z-50 translate-x-2 translate-y-[0.7px] shadow-2xl rounded-md">
+              <input type="radio" className='sr-only peer' id='surgery' name='dropdown'/>
+                    <p className="select-none h-20 relative flex items-center gap-1 rounded-3xl px-5 py-2 text-gray-500 hover:text-black transition-all duration-300 ease-in-out group-hover:text-first">Surgery <svg className="group-hover:rotate-180 group-hover:translate-y-[2px]" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg></p>
+                        <div className="DropdownMenuContent hidden hover:block group-hover:block absolute bg-white w-40 z-50 translate-x-2 -translate-y-2 shadow-2xl rounded-md">
 
-                            <p className="DropdownMenuItem h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
+                            <p className="DropdownMenuItem h-10 flex group2 items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
                                 Laparoscopic 
                             </p>
 
@@ -52,21 +49,26 @@ function Header() {
                             </p>
 
                             <p className="DropdownMenuItem h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer rounded-md hover:translate-x-1 transition-all duration-150">
-                                Diabetes
+                                Bariatrics
                             </p>
 
                          </div>
               {/* <p className="select-none rounded-3xl px-5 py-2 text-gray-500 hover:text-black peer-checked:text-white peer-checked:bg-first transition-all duration-300 ease-in-out">About</p>  */}
             </label>
 
-            <label htmlFor="blog" className='hover:cursor-pointer'>
-              <input type="radio" className='sr-only peer' id='blog' name='navigator'/>
-              <p className=" relative before:content-[''] before:bg-first before:w-0 before:h-[2px] before:absolute before:left-0 before:bottom-[-10px] before:rounded-md hover:before:w-full before:transition-all before:duration-300 select-none rounded-3xl px-5 py-2 text-gray-500 hover:text-black peer-checked:text-first peer-checked:font-semibold peer-checked:bg-opacity-45 transition-all duration-300 ease-in-out peer-checked:before:w-full">Blogs</p> 
+            <label htmlFor="ip" className='hover:cursor-pointer'>
+              <input type="radio" className='sr-only peer' id='ip' name='navigator'/>
+              <p className=" relative  before:content-[''] before:bg-first before:w-0 before:h-[2px] before:absolute before:left-0 before:bottom-[-10px] before:rounded-md hover:before:w-full before:transition-all before:duration-300 select-none rounded-3xl px-5 py-2 text-gray-500 hover:text-black peer-checked:text-first peer-checked:font-normal peer-checked:bg-opacity-45 transition-all duration-300 ease-in-out peer-checked:before:w-full">International Patient</p> 
             </label>
 
+            <label htmlFor="blog" className='hover:cursor-pointer'>
+              <input type="radio" className='sr-only peer' id='blog' name='navigator'/>
+              <p className="select-none  relative before:content-[''] before:bg-first before:w-0 before:h-[2px] before:absolute before:left-0 before:bottom-[-10px] before:rounded-md hover:before:w-full before:transition-all before:duration-300 rounded-3xl px-5 py-2 text-gray-500 hover:text-black peer-checked:bg-opacity-45 transition-all duration-300 ease-in-out peer-checked:text-first peer-checked:font-semibold peer-checked:before:w-full"> Blogs </p> 
+            </label>
+            
             <label htmlFor="contact" className='hover:cursor-pointer'>
               <input type="radio" className='sr-only peer' id='contact' name='navigator'/>
-              <p className="select-none relative before:content-[''] before:bg-first before:w-0 before:h-[2px] before:absolute before:left-0 before:bottom-[-10px] before:rounded-md hover:before:w-full before:transition-all before:duration-300 rounded-3xl px-5 py-2 text-gray-500 hover:text-black peer-checked:bg-opacity-45 transition-all duration-300 ease-in-out peer-checked:text-first peer-checked:font-semibold peer-checked:before:w-full">Contact Us</p> 
+              <p className="select-none  relative before:content-[''] before:bg-first before:w-0 before:h-[2px] before:absolute before:left-0 before:bottom-[-10px] before:rounded-md hover:before:w-full before:transition-all before:duration-300 rounded-3xl px-5 py-2 text-gray-500 hover:text-black peer-checked:bg-opacity-45 transition-all duration-300 ease-in-out peer-checked:text-first peer-checked:font-semibold peer-checked:before:w-full">Contact Us</p> 
             </label>
           </div>
         </nav>
