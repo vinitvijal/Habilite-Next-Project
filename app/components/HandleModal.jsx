8ffbd86d71e839a77d426209b6956609ea80ed1e
Vinu <vineet.22566@sscbs.du.ac.in>
2025-01-10 11:56:44 +0530

    header and hero section corrected

--- app/components/HandleModal.jsx
+++ app/components/HandleModal.jsx
@@ -10,7 +10,7 @@ function HandleModal() {
 
   return (
     <>
-    {!showModal?<button className='group fixed z-20 max-sm:hover:scale-105 transition-all duration-300 sm:bottom-10 bottom-6 right-6 sm:right-10 bg-first flex items-center rounded-lg p-2 text-white sm:text-xl font-semibold shadow-sm shadow-black ' onClick={()=>{setShowModal(true)}} >
+    {!showModal?<button className='group fixed z-20 max-sm:hover:scale-105 transition-all duration-300 sm:bottom-10 bottom-6 right-6 sm:right-10 bg-first flex items-center rounded-lg p-2 text-white sm:text-xl font-semibold shadow-lg  ' onClick={()=>{setShowModal(true)}} >
       <FaBook className='text-white max-sm:hidden size-8 sm:my-1 mx-2' />
       <div className=' sm:overflow-hidden sm:transition-all  sm:duration-500 sm:ease-in-out w-auto sm:max-w-0 group-hover:sm:max-w-xs group-hover:duration-500 sm:inline-block' ><span className="flex items-center  w-full whitespace-nowrap">Book Appointment <FaArrowRightLong className='size-4 text-white ml-1 pt-1 ' /> </span></div>
     </button>:null}
--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,106 +1,59 @@
+'use client'
 // Hero.jsx
 import React from 'react';
+import { Fade } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+
+
+
+const fadeImages = [
+  {
+    url: '/images/banner-1.webp',
+  },
+  {
+    url: '/images/banner-2.webp',
+  },
+  {
+    url: '/images/banner-3.webp',
+  },
+  {
+    url: '/images/banner-4.webp',
+  },
+  {
+    url: '/images/banner-5.webp',
+  }
+];
 
 const Hero = () => {
   return (
-    <div className=" md:h-[75vh] h-screen  bg-black relative md:mb-80 mb-[80vh] md:px-0 px-10 pb-12 sm:pb-0">
+    <div className="relative md:px-0 sm:pb-0 ">
+      <div className="slide-container h-full">
+      <Fade arrows={false}
+        duration={2000}
+        transitionDuration={1000}
+        infinite={true}
+        pauseOnHover={true}
+
+        >
+        {fadeImages.map((fadeImage, index) => (
+          <div key={index}>
+            <img src={fadeImage.url} />
+          </div>
+        ))}
+      </Fade>
+    </div>
+      <div>
+        <Calculator />
+      </div>
+
       
-<ul className="slideshow">
-	<li><span></span></li>
-  <li><span></span></li>
-	<li><span></span></li>
-	<li><span></span></li>
-	<li><span></span></li>
-</ul>
-<div>
-  <Calculator />
-</div>
-
-
-      <style>
-        {`
-          .slideshow {
-  list-style-type: none;
-}
-
-/** SLIDESHOW **/
-.slideshow,
-.slideshow:after { 
-    width: 100%;
-    height: 100%;
-    left: 0px;
-    z-index: 0; 
-}
-
-.slideshow li span { 
-		position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0px;
-    left: 0px;
-    color: transparent;
-    background-size: cover;
-    background-position: 50% 50%;
-    background-repeat: no-repeat;
-    opacity: 0;
-    z-index: 0;
-    animation: imageAnimation 30s linear infinite 0s; 
-}
-
-
-
-.slideshow li:nth-child(1) span { 
-    background-image: url("/images/banner-1.webp"); 
-}
-.slideshow li:nth-child(2) span { 
-    background-image: url("/images/banner-2.webp");
-    animation-delay: 6s; 
-}
-.slideshow li:nth-child(3) span { 
-    background-image: url("/images/banner-3.webp");
-    animation-delay: 12s; 
-}
-.slideshow li:nth-child(4) span { 
-    background-image: url("/images/banner-4.webp");   
-    animation-delay: 18s; 
-}
-.slideshow li:nth-child(5) span { 
-    background-image: url("/images/banner-5.webp");
-    animation-delay: 24s; 
-}
-
-
-
-@keyframes imageAnimation { 
-    0% { opacity: 0; animation-timing-function: ease-in; }
-    8% { opacity: 1; animation-timing-function: ease-out; }
-    17% { opacity: 1 }
-    25% { opacity: 0 }
-    100% { opacity: 0 }
-}
-
-
-@keyframes titleAnimation { 
-    0% { opacity: 0 }
-    8% { opacity: 1 }
-    17% { opacity: 1 }
-    19% { opacity: 0 }
-    100% { opacity: 0 }
-}
-
-
-.no-cssanimations .cb-slideshow li span {
-	opacity: 1;
-}
-        `}
-      </style>
     </div>
   );
 };
 
 const Calculator = () => {
   return (
-    <div className=' pt-8 md:mt-13 mt-12 md:px-40 px-2 flex justify-center pb-20 '>
+    <div className=' pt-8 md:mt-13 mt-12 md:px-40 px-10 flex justify-center pb-20 '>
       <div className='grid md:grid-cols-3 grid-cols-1 md:gap-20 gap-10'>
         {/* BMI Calculator */}
         <div>
