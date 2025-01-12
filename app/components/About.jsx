6529ebc33b85328e20c2ce18c3b96169c611435e
roshni <roshni.23545@sscbs.du.ac.in>
2025-01-12 15:02:42 +0530

    added about section complete

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,9 +1,15 @@
+'use client'
 import React from 'react'
 import * as Separator from "@radix-ui/react-separator";
+import { motion } from "motion/react";
 
 function About() {
   return (
-    <section className='h-fit px-5 lg:px-32 py-5 w-full'>
+    <section className='h-full px-5 lg:px-32 py-5 w-full'>
+      <motion.div initial={{ opacity: 0, x: -100 }} 
+          whileInView={{ opacity: 1, x: 0 }} 
+          viewport={{ amount: 0.3 }} 
+          transition={{ duration: 0.8 }} >
       <p className='text-first text-3xl lg:text-5xl mb-4 text-left'>About Us</p>
       <Separator.Root orientation="horizontal" className="mx-0 w-1/4 lg:w-1/12 h-[1px] bg-third" />
 
@@ -27,18 +33,7 @@ function About() {
       <div className="flex justify-center lg:justify-start">
         <button className='mt-8 w-28 p-[6px] duration-300 border-first border-[1px] rounded-md hover:bg-first hover:text-white text-first'>Know More</button>
       </div>
-
-      <div className="lower-content  mt-14">
-        <p className='text-xl lg:text-3xl text-second text-left'>Best Laparoscopic, Laser and Weight Loss Hospital in Delhi</p>
-
-        <div className="mt-8 lg:w-3/4">
-          <p className='mt-5 text-sm lg:text-base'><span className='font-semibold'>Habilite Clinics</span>, one of the best hospital for laparoscopic and laser surgery in New Delhi, is well renowned for its comprehensive services in weight management, advanced laparoscopic surgeries, and treatment of various anorectal disorders.</p>
-          <p className='mt-5 text-sm lg:text-base'>Located in South Delhi, Habilite Clinics is distinguished by its multidisciplinary approach, assembling a team of highly experienced and internationally trained surgeons, dieticians, and medical professionals all under one roof. Their collaboration ensures that patients receive personalized care tailored to their unique needs and aspirations.</p>
-          <p className='mt-5 text-sm lg:text-base'>Habilite Clinics has established itself as one of the best centres for weight loss. For those looking for the best and sustainable weight loss solutions, the clinic offers more than just a solution - it offers a transformational experience.</p>
-          <p className='mt-5 text-sm lg:text-base'>Habilite Clinics offers a complete range of treatment modalities including advanced laser surgeries for the management of various anorectal disorders, circumcision, and treatment of lipoma. Habilite Clinics crafts personalized care plans that mitigate recurrence and minimize discomfort, ensuring a swift and smooth recovery for every patient.</p>
-          <p className='mt-5 text-sm lg:text-base'>Habilite clinics also offer advanced laparoscopic surgeries for the management of gallstones, hernia, acute appendicitis, and rectal prolapse. From initial assessments to post-operative care by a multi-disciplinary team, patients benefit from the latest surgical techniques administered with precision and finesse.</p>
-        </div>
-      </div>
+      </motion.div>
     </section>
   )
 }
