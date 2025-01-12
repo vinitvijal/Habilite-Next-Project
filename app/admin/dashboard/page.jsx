4c36caf85dd87e33f4854493680cbb6c80eef295
Vinu <vineet.22566@sscbs.du.ac.in>
2025-01-12 12:44:41 +0530

    changes

---
+++ app/admin/dashboard/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        Dashboard
+
+        Book Appointment
+        View Appointments
+    </div>
+  )
+}
+
+export default page
---
+++ app/admin/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      Login Page
+    </div>
+  )
+}
+
+export default page
