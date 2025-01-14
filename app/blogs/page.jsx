08f9b1e0fbc6e9b61067f6d36f87267d72b7a557
Namiiii-sys <Namitamehra207@gmail.com>
2025-01-14 20:31:29 +0530

    All blogs commit 1

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -1,215 +1,231 @@
 import React from "react";
 import Image from "next/image";
-import main from "@/public/main.webp"
+import { motion } from "framer-motion";
+import calendar from "@/public/calendar.svg";
+import drkapil from "@/public/drkapil.png";
+import alt from "@/public/alt.png"
 
-
-
-
-function Blog(){
+function Blog() {
   const featured = [ 
     {
-     id: 1,
-     title: "XYZ BSB DFNH",
-     content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga illum deleniti sed amet itaque distinctio possimus animi, vitae nihil cupiditate at quas voluptates cum, consequuntur odio repellendus soluta quasi dolor. Ullam laudantium totam quam animi nisi ipsam, tempore ad reprehenderit tenetur sint!",
-     imageUrl: "https://www.habiliteclinics.com/assets/images/home/blog-4.webp", 
-     author: "Dr. XYZ",
-    
+      id: 1,
+      title: "SILS-Single Incision Laparoscopic Surgery",
+      content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga illum deleniti sed amet itaque distinctio possimus animi, vitae nihil cupiditate at quas voluptates cum, consequuntur odio repellendus soluta quasi dolor. Ullam laudantium totam quam animi nisi ipsam, tempore ad reprehenderit tenetur sint!",
+      date: "8 May, 2024",
+      author: "Dr. kapil Agrawal",
     },
   ];
 
   const blogs = [
     {
       id: 1,
-      title: "How to Choose the Right Hospital",
-      excerpt: "Many state-of-the-art hospitals in Delhi provide advanced treatments...",
-      date: "September 30, 2024",
-      author: "Dr. XYZ",
-      imageUrl: "https://www.habiliteclinics.com/assets/images/home/blog-4.webp", 
-
+      title: "Which is the Best Bariatric Surgery",
+      excerpt: "Weight Loss Surgery is a life-changing procedure that can help someone",
+      date: "March 14, 2024",
+      author: "Dr. Kapil Agrawal",
+      imageUrl: "https://www.habiliteclinics.com/assets/images/home/blog-1.webp",
     },
-
     {
       id: 2,
-      title: "How to Choose the Right Hospital?",
-      excerpt: "Choosing the right hospital is crucial for effective treatment...",
-      date: "October 5, 2024",
-      author: "Dr. XYZ",
-      imageUrl: "https://www.habiliteclinics.com/assets/images/home/blog-4.webp", 
+      title: "Bariatric Surgery Cost in Delhi, India",
+      excerpt: "Bariatric surgery is a transformative and life altering procedure meant for",
+      date: "October 12, 2024",
+      author: "Dr. Kapil Agrawal",
+      imageUrl: "https://www.habiliteclinics.com/assets/images/home/blog-2.webp",
     },
-
     {
       id: 3,
-      title: "How to Choose the Right Hospital",
-      excerpt: "Choosing the right hospital is crucial for effective treatment...",
+      title: "SILS-Single Incision Laparoscopic Surgery",
+      excerpt: "Single-incision laparoscopic surgery is one",
       date: "October 8, 2024",
-      author: "Dr. XYZ",
-      imageUrl: "https://www.habiliteclinics.com/assets/images/home/blog-4.webp", 
-    },
-
-    {
-      id: 4,
-      title: "How to Choose the Right Hospital",
-      excerpt: "Many state-of-the-art hospitals in Delhi provide advanced treatments...",
-      date: "September 30, 2024",
-      author: "Dr. XYZ",
-      imageUrl: "https://www.habiliteclinics.com/assets/images/home/blog-4.webp", 
-    },
-
-    {
-      id: 5,
-      title: "How to Choose the Right Hospital?",
-      excerpt: "Choosing the right hospital is crucial for effective treatment...",
-      date: "October 5, 2024",
-      author: "Dr. XYZ",
-      imageUrl: "https://www.habiliteclinics.com/assets/images/home/blog-4.webp", 
-    },
-
-    {
-      id: 6,
-      title: "How to Choose the Right Hospital",
-      excerpt: "Choosing the right hospital is crucial for effective treatment...",
-      date: "October 8, 2024",
-      author: "Dr. XYZ",
-      imageUrl: "https://www.habiliteclinics.com/assets/images/home/blog-4.webp", 
+      author: "Dr. Kapil Agrawal",
+      imageUrl: "https://www.habiliteclinics.com/assets/images/home/blog-4.webp",
     },
   ];
 
+  const popularBlogs = [
+    {
+      id: 1,
+      title: "How to Maintain Weight Loss After Bariatric Surgery",
+      excerpt: "Post-surgery care is as important as the procedure itself.",
+      date: "August 15, 2024",
+      author: "Dr. Kapil Agrawal",
+      imageUrl: "https://www.habiliteclinics.com/assets/images/home/blog-3.webp",
+    },
+    {
+      id: 2,
+      title: "Diet Tips Post the Bariatric Surgery and Effects.",
+      excerpt: "Learn how to maintain a healthy diet after your surgery.Post-surgery care is as important",
+      date: "July 20, 2024",
+      author: "Dr. Kapil Agrawal",
+      imageUrl: "https://www.habiliteclinics.com/assets/images/home/blog-4.webp",
+    },
+  ];
 
   return (
     <> 
-    {/* search Bar */}
-
-
-    <div className="max-w-6xl mx-auto p-6 my-12">
-      <div className="w-5/6 h-16 shadow-lg p-4 mb-20 m-auto rounded-md flex justify-between items-center">
-        <input className="px-4 py-2 m-6 text-xl rounded-md outline-none" type="text" aria-label="Search our blog..." placeholder="Search our blog..." />
-        <button className="px-5 py-2 w-auto mr-2 bg-first rounded-lg text-white font-semibold text-xl hover:scale-105 shadow-sm hover:shadow-none">
-          Search
-        </button>
-      </div>
-
       {/* Featured blog */}
+     
+      <div className="max-w-7xl mx-auto m-12">
+        <div className="flex justify-center items-center">
+          <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-12">
+            {featured.map((blog) => (
+              <div key={blog.id} className="bg-white shadow-lg rounded-lg overflow-hidden hover:-translate-y-5 transition-transform duration-500 mt-4 ">
+                <div className="flex flex-row justify-center items-center relative">
+                  <div className="sec-1 relative object-cover w-2/5" >
+                    <Image
+                      src={alt}
+                      alt="Blog Image"
+                      className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300 curved-image-2" />  
+                  </div>
 
-      <div className="flex justify-center items-center">
-        <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-12">
-          {featured.map((blog) => (
-            <div key={blog.id} className="bg-white shadow-md rounded-lg overflow-hidden hover:shadow-lg hover:-translate-y-5 transition-transform duration-500 mt-4 p-4 pb-1 border-1">
-              <div className="p-4">
-                <div className="relative">
-                  <img
-                    src={main}
-                    alt="Blog Image"
-                    width={800}
-                    height={400}
-                    className="w-full h-48 object-cover group-hover:scale-105 transition-transform duration-300" />
-                  <div className="absolute top-2 right-2 bg-first text-white text-sm font-semibold px-2 py-2 rounded">
-                    FEATURED
+                  <div className="sec-2 w-3/5 px-10 mt-10">
+                  <div className="bg-first absolute top-4 right-6 w-20 text-white text-sm font-semibold px-2 py-2 rounded">
+                      FEATURED
+                  </div>
+                  <h2 className="mt-4 mb-6 text-lg font-bold text-black">
+                    {blog.title}
+                  </h2>
+                  <p className="text-sm text-black mt-2">{blog.content}</p>
+                  <button className="mt-4 px-2 py-1 rounded-md bg-first text-white font-xs font-semibold hover:bg-second hover-scale-105 ">
+                    Read More
+                  </button>
+                  
+                  <div className="w-full flex items-center justify-start text-sm text-black font-semibold px-5 py-3 mt-2 mb-1 gap-2 relative bottom-0">
+                    <div className="w-8 h-8 ml-0 mt-10 rounded-full bg-gray-300 overflow-hidden bottom-0">
+                      <Image
+                        src={drkapil} 
+                        alt="Author"
+                        width={60}
+                        height={60}
+                        className="object-cover" />
+                    </div>
+                    <span className="ml-0 mt-10 font-semibold text-md">{blog.author}</span>
+                    <div className="flex gap-2 mt-10 ml-4">
+                    <Image src={calendar} className="z-0 h-4 w-6" alt="none"></Image>
+                    <div className="text-black text-sm">{blog.date}</div>
+                </div>
                   </div>
                 </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
 
-                <h2 className="mt-4 text-lg font-bold text-black">
-                  {blog.title}
-                </h2>
-                <p className="text-sm text-black mt-2">{blog.content}</p>
-                <button className="mt-4 text-fourth font-medium hover:underline">
-                  Read More
-                </button>
-                <div className="flex items-center">
-                  <div className="w-8 h-8 p-4 m-4 rounded-full bg-gray-300 overflow-hidden">
-                    <img
-                      src={blog.imageUrl}
-                      alt="Author"
-                      width={40}
-                      height={40}
-                      className="object-cover" />
-                  </div>
-                  <span className="ml-0">{blog.author}</span>
+        {/* Latest blogs */}
+        <h1 className="text-4xl font-bold text-second pt-10 mb-6 mt-10 text-center">LATEST BLOGS</h1>
+        <h1 className="text-xl font-semibold text-first mb-6 pb-8 text-center">Here's what we've been up to lately.</h1>
+        <div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-12 m-3">
+          {blogs.map((blog) => (
+            <div key={blog.id} className="relative flex sm:w-full flex-col items-center shadow-lg rounded-lg hover:shadow-lg hover:scale-105 transition-transform duration-300 hover:bg-gray-200 m-6 mb-0 cursor-pointer">
+              <img src={blog.imageUrl} alt={blog.title} className="w-full rounded-t-lg" />
+              <div className="p-2 px-4 max-w-6xl">
+                <div className="flex justify-start gap-2 px-1 py-2">
+                  <Image src={calendar} className="z-0 h-4 w-4" alt="none"></Image>
+                  <div className="text-black text-sm">{blog.date}</div>
                 </div>
 
+                <h2 className="mt-2 p-1 text-lg font-bold text-black">{blog.title}</h2>  
+                <p className="text-sm text-second p-1">{blog.excerpt}</p>
 
+                <div className="flex justify-center items-center">
+                  <button className="mt-6 px-3 py-2 rounded-md bg-first text-white font-xs font-semibold hover:bg-second hover-scale-105 ">
+                    Read More
+                  </button>
+                </div>
+              </div>
+
+              <div className="w-full flex items-center justify-start text-sm text-black font-semibold px-5 py-3 mt-2 mb-1 gap-2 relative bottom-0">
+                <div className="relative bottom-0 w-8 h-8 ml-0 rounded-full bg-gray-300 overflow-hidden ">
+                  <Image
+                    src={drkapil} 
+                    alt="Author"
+                    width={60}
+                    height={60}
+                    className="object-cover" />
+                </div>
+                <span className="ml-0 font-semibold text-md">{blog.author}</span>
               </div>
             </div>
           ))}
         </div>
 
-        {/* Latest blogs */}
+        {/* Popular blogs */}
+        <div className="categories-popular flex flex-row justify-center items-center gap-16 h-full m">
+         <div className="w-3/4 mr-4">
+        <h1 className="text-4xl font-bold text-second mb-6 pt-8 my-10 text-center ">POPULAR BLOGS</h1>
+        <h1 className="text-xl font-semibold text-first mb-6 pb-8 text-center">Our most loved content by readers.</h1>
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-14 m-3 rounded-lg">
+          {popularBlogs.map((blog) => (
+            <div key={blog.id} className="relative flex sm:w-full flex-col items-center rounded-lg shadow-lg hover:shadow-lg hover:scale-105 transition-transform duration-300 hover:bg-gray-200 hover:rounded-t-lg m-6 mb-0 cursor-pointer">
+              <img src={blog.imageUrl} alt={blog.title} className="w-full rounded-t-lg" />
+              <div className="p-2 px-4 max-w-6xl">
+                <div className="flex justify-start gap-2 px-1 py-2">
+                  <Image src={calendar} className="z-0 h-4 w-4" alt="none"></Image>
+                  <div className="text-black text-sm">{blog.date}</div>
+                </div>
+                <h2 className="mt-2 p-1 text-lg font-bold text-black">{blog.title}</h2>  
+                <p className="text-sm text-second p-1">{blog.excerpt}</p>
+                <div className="flex justify-center items-center">
+                  <button className="mt-6 px-3 py-2 rounded-md bg-first text-white font-xs font-semibold hover:bg-second hover-scale-105 ">
+                    Read More
+                  </button>
+                </div>
+              </div>
 
-        <div className="p-2">
-          <img src={featured.imageUrl} alt={featured.title} className="w-full h-42 object-cover" />
-        </div>
-      </div>
-
-      <h1 className="text-4xl font-bold text-black mb-6 pt-10 my-20 p-3">Latest Blogs</h1>
-      <h1 className="text-xl font-semibold text-third mb-6 pb-4 p-3">Here's what we've been to lately </h1>
-      <div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-12 m-3">
-        {blogs.map((blog) => (
-          <div key={blog.id} className="bg-white shadow-md rounded-lg overflow-hidden hover:shadow-lg hover:scale-105 transition-transform duration-300">
-            <img src={blog.imageUrl} alt={blog.title} className="w-full h-48 object-cover" />
-            <div className="p-4">
-
-              <h2 className="mt-4 text-lg font-bold text-black">
-                {blog.title}
-              </h2>
-              <p className="text-sm text-black mt-2">{blog.excerpt}</p>
-              <div className="mt-3 text-third text-sm">{blog.date}</div>
-              <button className="mt-4 text-fourth font-medium hover:underline">
-                Read More
-              </button>
-            </div>
-            <div className="flex items-center justify-between text-sm text-gray-500">
-              <div className="flex items-center">
-                <div className="w-8 h-8 p-4 m-4 rounded-full bg-gray-300 overflow-hidden">
+              <div className="w-full flex items-center justify-start text-sm text-black font-semibold px-5 py-3 mt-2 mb-1 gap-2 relative bottom-0">
+                <div className="relative bottom-0 w-8 h-8 ml-0 rounded-full bg-gray-300 overflow-hidden ">
                   <Image
-                    src="/public/main.webp" 
+                    src={drkapil} 
                     alt="Author"
-                    width={40}
-                    height={40}
+                    width={60}
+                    height={60}
                     className="object-cover" />
                 </div>
-                <span className="ml-0">{blog.author}</span>
+                <span className="ml-0 font-semibold text-md">{blog.author}</span>
               </div>
+            </div>
+          ))}
+        </div>
+         </div>
+         <div className="w-1/4 flex-col justify-center items-center rounded-2xl m-auto ">
+         {/* <h1 className="text-second font-bold text-center text-4xl p-2 pt-2"> CATEGORIES </h1> */}
+          <div className="mt-40 mb-20 shadow-lg rounded-lg ml-12">
+           <div className="w-full h-16 px-4 bg-second rounded-t-2xl font-semibold flex justify-center items-center text-xl sm:text-lg">
+            <div className="text-white">
+              <a href="#">CATEGORIES</a>
             </div>
            </div>
-            
-        ))}
-      </div>
-       
-       {/* popular blogs */}
-       
-      <div className="container mt-12 h-[90vh] w-full ml-0" >
-          <h1 className="text-4xl font-bold text-black mb-6 pb-4 p-3 ml-0">Popular Blogs</h1>
-          <div className="w-full flex justify-between items-center text-xl font-semibold mb-6 pb-4 p-3 gap-8">
-            <div className="w-1/2 h-[60vh] flex-col-2 p-4 m-8 shadow-lg rounded-lg overflow-hidden hover:shadow-lg hover:scale-105 transition-transform duration-300 mt-4 ml-0">
-            <div className="h-3/5 p-2 bg-third">
-              <img src="img" alt="pop-blog" />
-            </div>
-            <div className="h-2/5 text-sm px-2 py-1 ">
-              <div className="px-2 flex gap-2">
-                <img className="h-6 w-6 rounded-full bg-third m-1" src="img.jpg" alt="img" /> 
-                <h1 className="m-2">DR. XYZ</h1> 
+           <div className="img w-full justify-center items-center container my-8 p-4 h-[580px]">
+              <div className="flex w-full flex-col flex-1 justify-center items-center gap-2">
+                <div className="flex justify-center w-full">
+                  <a className="text-lg sm:text-md font-medium text-second/70 hover:text-second hover:scale-105 duration-300 text-center" href="#">Bariatric surgery</a>
+                </div>
+                <div data-orientation="horizontal" role="separator" class="w-3/4 h-[1px] bg-third"></div>
+                <div className="flex justify-center w-full">
+                    <a className="text-lg sm:text-md font-medium text-second/70 hover:text-second hover:scale-105 duration-300 text-center" href="#">Non-surgical weight loss program</a>
+                </div>
+                <div data-orientation="horizontal" role="separator" class="w-3/4 h-[1px] bg-third"></div>
+                <div className="flex justify-center w-full">
+                      <a className="text-lg sm:text-md font-medium text-second/70 text-second hover:scale-105 duration-300 text-center" href="#">Surgery for diabetes</a>
+                </div>
+                <div data-orientation="horizontal" role="separator" class="w-3/4 h-[1px] bg-third"></div>
+                <div className="flex justify-center w-full">
+                        <a className="text-lg sm:text-md font-medium text-second/70 hover:text-second hover:scale-105 duration-300 text-center" href="#">Intragastric Balloon</a>
+                </div>
               </div>
-              <p className="p-2 pb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus impedit consequatur deserunt modi officia eveniet similique soluta, doloribus, ratione nulla non vero incidunt nam doloremque?</p>
-            </div>
-            </div>
-
-            <div className="w-1/2 h-[60vh] flex-col-2 p-4 m-8 shadow-lg rounded-lg overflow-hidden hover:shadow-lg hover:scale-105 transition-transform duration-300 mt-4 ml-0">
-            <div className="h-3/5 p-2 bg-third">
-              <img src="img" alt="pop-blog" />
-            </div>
-            <div className="h-2/5 text-sm px-2 py-1 ">
-              <div className="px-2 flex gap-2">
-                <img className="h-6 w-6 rounded-full bg-third m-1" src="img.jpg" alt="img" /> 
-                <h1 className="m-2">DR. XYZ</h1> 
-              </div>
-              <p className="p-2 pb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus impedit consequatur deserunt modi officia eveniet similique soluta, doloribus, ratione nulla non vero incidunt nam doloremque?</p>
-            </div>
-            </div>
+           </div>
           </div>
+         </div>
+        </div>
+
       </div>
-    </div>
+    
     </>
+   
   );
-  
 };
 
 export default Blog;
+
