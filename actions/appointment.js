a56ea69fddb31f8479d3444563ecd6d97ea996d7
vineet <vineet.22566@sscbs.du.ac.in>
2025-01-20 16:59:51 +0530

    api and creation done

---
+++ actions/appointment.js
@@ -0,0 +1,34 @@
+'use server'
+import { PrismaClient } from "@prisma/client"
+import { Validate } from "./auth"
+
+const prisma = new PrismaClient()
+
+export async function createAppointment(name= "", email, phone="", query= "" , date= "", time= "", type= "", status= "", remarks= ""){
+    
+    const res = await prisma.bookAppointment.create({
+        data: {
+            appointName: name,
+            appointPhone: phone,
+            appointQuery: query,
+            allotedDate: date,
+            allotedRemark: remarks,
+            allotedTime: time,
+            appointStatus: status,
+            allotedType: type,
+            appointEmail: email,
+        }
+    })
+    return res
+}
+
+export async function getAppointments(token, page=0){
+    const validation = await Validate(token);
+    if(!validation){
+        return "Invalid Authentication";
+    }
+    const response = await prisma.bookAppointment.findMany({
+        take: 10,
+        skip: page*10
+    })
+}
