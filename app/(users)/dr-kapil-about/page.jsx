b9744c6f34069bcd0f21a0266ef612f420ebddb8
Harshit <harshitkumar100436@gmail.com>
2025-01-28 23:39:07 +0530

    started

--- app/(users)/dr-kapil-about/page.jsx
+++ app/(users)/dr-kapil-about/page.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
-function page() {
+function drKapilAbout() {
   return (
     <div>
-      
+      <div>
+        <h1>Dr. Kapil Agrawal – Best Bariatric Surgeon in Delhi</h1>
+        <h3>MBBS, MS (SURGERY), MRCS (LONDON, U.K), MMED (SINGAPORE), FMAS</h3>
+        <h3>Senior Consultant, Laparoscopic & Bariatric Surgeon</h3>
+      </div>
+      <div>
+        <p>Dr. Kapil Agrawal is one of the top choices for finding the best Bariatric, Metabolic and Laparoscopic surgeons in Delhi, India. As one of the region's most respected Bariatric and Laparoscopic surgeons, he is well known for communicating effectively with his patients and utilizing innovative technology. He is also known for tailoring treatment plans to his patients' needs and surgical precision.</p>
+
+          <p>He received his training at some of the best and most prominent surgical institutes in India and abroad. He was associated with North Middlesex University Hospital, Edmonton, U.K, and the Aintree University Hospital, Liverpool. He was honored with an MRCS (Member of Royal College of Surgeons, London, UK) in 2007 in England.</p>
+
+          <p>His area of expertise includes advanced laparoscopic gastrointestinal surgeries as well as bariatric or weight-loss surgeries. Along with his team, he has helped thousands of patients with deflated self-esteem and inflated BMI, getting rid of obesity permanently.
+          </p>
+      </div>
+
+
     </div>
   )
 }
 
-export default page
+export default drKapilAbout
