1a4faad926887d41db21ecbe0c9901c358bf76ba
soubhagyacodes <soubhagya613mishra@gmail.com>
2025-01-29 17:20:06 +0530

    Design Changes

--- app/(users)/blogs/page.jsx
+++ app/(users)/blogs/page.jsx
@@ -49,25 +49,36 @@ function Blog() {
 
   return (
     <>
-      {/* <div className="bg-white h-28 w-full shadow-md relative top-0 hidden"></div> */}
-    <div className="page md:pl-24 px-5 md:px-0">
+      <section className="title mt-24 flex flex-col">
+        <p className="mx-auto font-medium text-lg">The Blogs</p>
+        <p className="md:text-7xl text-6xl mx-auto mt-2 font-bold">Trusted Medical Insights</p>
+        <p className="text-first md:text-2xl text-lg mx-auto mt-4 font-semibold">Everything you need to know about health, all in one place.</p>
+      </section>
 
-      <section className="title mt-24 flex flex-col gap-4">
-        <p className="md:text-8xl text-6xl">Blogs</p>
-        <p className="text-first md:text-2xl text-lg">Insights and experiences to inspire better health.</p>
+      <section className="categories md:mt-20 mt-14 text-2xl flex flex-col gap-5 items-start mx-auto">
+        <div className="pills text-sm gap-2 md:flex md:items-center grid grid-cols-1">
+          {categories.map((cat) => {
+            return (
+            <Link key={cat} href={`/blogs/category/${categories.indexOf(cat)}`}>
+            <p className="select-none text-second px-3 py-1 rounded-lg bg-[#a1bef534] hover:bg-[#a1bef593] cursor-pointer active:bg-[#7a9ee0fe] border-[1px] border-first">{cat}</p>
+            </Link>
+          )})}
+        </div>
       </section>
 
 
-      <section className="featured mt-28">
-        <p className="md:text-4xl text-3xl">Featured <span className="text-first">Blogs</span></p>
-        <p className="text-gray-500 text-lg font-light mt-1">Spotlighting the best reads for you.</p>
-        <Separator.Root orientation="horizontal" className="mx-0 my-4 w-1/4 lg:w-1/12 h-[1.5px] bg-third animate-shake md:block hidden" />
+    <div className="page md:px-60 px-5 xl:px-14">
 
-        <div className="boxes mt-10 md:mr-24">
+      <section className="featured mt-20">
+        {/* <p className="md:text-4xl text-3xl">Featured <span className="text-first">Blogs</span></p>
+        <p className="text-gray-500 text-lg font-light mt-1">Spotlighting the best reads for you.</p>
+        <Separator.Root orientation="horizontal" className="mx-0 my-4 w-1/4 lg:w-1/12 h-[1.5px] bg-third animate-shake md:block hidden" /> */}
+
+        <div className="boxes mt-10 ">
           {featured1 && (
             <Link href={`/blogs/${featured1.slug}`}>
               {/* BG */}
-              <div className="overflow-clip group md:overflow-auto first-featured will-change-transform md:h-[70vh] h-[40vh] group bg-cover md:bg-center bg-left rounded-lg relative ease-in-out md:hover:border-2 md:hover:border-first md:hover:rounded-lg md:hover:scale-[101%] cursor-pointer transition-all duration-300 box-border md:hover:shadow-xl" style={{ backgroundImage: `url(${featured1.blogImage})` }}>
+              <div className="overflow-clip group md:overflow-auto first-featured will-change-transform md:h-[70vh] lg:h-[75vh] h-[40vh] group bg-cover md:bg-center bg-left rounded-lg relative ease-in-out md:hover:border-2 md:hover:border-first md:hover:rounded-lg md:hover:scale-[101%] active:scale-100 cursor-pointer transition-all duration-300 box-border md:hover:shadow-xl" style={{ backgroundImage: `url(${featured1.blogImage})` }}>
                 <div className="overlay inset-0 z-10 bg-gradient-to-b from-transparent absolute to-[#00000a74] rounded-lg duration-300"></div>
 
                 <div className="content z-20 mt-auto duration-300">
@@ -102,9 +113,9 @@ function Blog() {
             </Link>
           )}
 
-          <div className="below-2-boxes grid md:grid-cols-2 grid-cols-1 gap-2 mt-3 ">
+          <div className="below-2-boxes grid md:grid-cols-2 grid-cols-1 gap-3 mt-3 ">
             {featured.slice(1).map((blog) => (
-              <div key={blog.id} className="box w-full will-change-transform md:h-[50vh] h-[25vh] group rounded-lg relative bg-cover md:bg-center bg-top md:hover:border-2 md:hover:border-first md:hover:scale-[101%] cursor-pointer transition-all duration-300 box-border md:hover:shadow-xl ease-in-out" style={{ backgroundImage: `url(${blog.blogImage})` }}>
+              <div key={blog.id} className="box w-full will-change-transform md:h-[328px] h-[25vh] group rounded-lg relative bg-cover md:bg-center bg-top md:hover:border-2 md:hover:border-first md:hover:scale-[101%] cursor-pointer transition-all duration-300 box-border md:hover:shadow-xl ease-in-out" style={{ backgroundImage: `url(${blog.blogImage})` }}>
                 <Link key={blog.slug} href={`/blogs/${blog.slug}`} passHref>
                   <div className="overlay inset-0 z-10 bg-gradient-to-b from-transparent absolute to-[#00000a74] rounded-lg duration-300"></div>
 
@@ -136,19 +147,7 @@ function Blog() {
         </div>
       </section>
 
-      <section className="categories md:mt-20 mt-14 text-2xl flex flex-col gap-5 items-start">
-        <p className="mb-1 items-center gap-1 font-light flex"><svg className="mr-1 mt-1 text-first" width="33" height="33" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.14921 3.99996C2.14921 2.97778 2.97784 2.14915 4.00002 2.14915C5.02219 2.14915 5.85083 2.97778 5.85083 3.99996C5.85083 5.02213 5.02219 5.85077 4.00002 5.85077C2.97784 5.85077 2.14921 5.02213 2.14921 3.99996ZM4.00002 1.24915C2.48079 1.24915 1.24921 2.48073 1.24921 3.99996C1.24921 5.51919 2.48079 6.75077 4.00002 6.75077C5.51925 6.75077 6.75083 5.51919 6.75083 3.99996C6.75083 2.48073 5.51925 1.24915 4.00002 1.24915ZM5.82034 11.0001L2.49998 12.8369V9.16331L5.82034 11.0001ZM2.63883 8.21159C2.17228 7.9535 1.59998 8.29093 1.59998 8.82411V13.1761C1.59998 13.7093 2.17228 14.0467 2.63883 13.7886L6.57235 11.6126C7.05389 11.3462 7.05389 10.654 6.57235 10.3876L2.63883 8.21159ZM8.30001 9.00003C8.30001 8.61343 8.61341 8.30003 9.00001 8.30003H13C13.3866 8.30003 13.7 8.61343 13.7 9.00003V13C13.7 13.3866 13.3866 13.7 13 13.7H9.00001C8.61341 13.7 8.30001 13.3866 8.30001 13V9.00003ZM9.20001 9.20003V12.8H12.8V9.20003H9.20001ZM13.4432 2.19311C13.6189 2.01737 13.6189 1.73245 13.4432 1.55671C13.2675 1.38098 12.9826 1.38098 12.8068 1.55671L11 3.36353L9.19321 1.55674C9.01748 1.381 8.73255 1.381 8.55682 1.55674C8.38108 1.73247 8.38108 2.0174 8.55682 2.19313L10.3636 3.99992L8.55682 5.80671C8.38108 5.98245 8.38108 6.26737 8.55682 6.44311C8.73255 6.61885 9.01748 6.61885 9.19321 6.44311L11 4.63632L12.8068 6.44314C12.9826 6.61887 13.2675 6.61887 13.4432 6.44314C13.6189 6.2674 13.6189 5.98247 13.4432 5.80674L11.6364 3.99992L13.4432 2.19311Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>Categories </p>
-        <div className="pills text-sm gap-2 md:flex md:items-center grid grid-cols-1">
-          {categories.map((cat) => {
-            return (
-            <Link key={cat} href={`/blogs/category/${categories.indexOf(cat)}`}>
-            <p className="select-none text-second px-3 py-1 rounded-lg bg-[#a1bef534] hover:bg-[#a1bef593] cursor-pointer active:bg-[#7a9ee0fe] border-[1px] border-first">{cat}</p>
-            </Link>
-          )})}
-        </div>
-      </section>
-
-      <section className="latest-blogs mt-20 md:mr-24">
+      <section className="latest-blogs mt-20 ">
         <div className="title-content flex justify-between md:items-center items-start">
           <div className="title">
             <p className="md:text-4xl text-3xl">Latest <span className="text-first">Blogs</span></p>
@@ -166,9 +165,9 @@ function Blog() {
         </div>
 
         <div className="content">
-          <div className="grid md:grid-cols-3 md:gap-8 grid-cols-1 gap-4 mt-10">
+          <div className="grid md:grid-cols-3 md:gap-6 lg:gap-4 3xl:grid-cols-4 3xl:gap-4 grid-cols-1 gap-4 mt-10">
             {blogs.slice(0, 6).map((blog) => (
-              <div key={blog.id} className="w-full select-none relative h-[65vh] bg-white rounded-lg flex flex-col shadow-lg md:hover:scale-[102%] transition-all duration-300">
+              <div key={blog.id} className="w-full select-none relative lg:h-[68vh] bg-white rounded-lg flex flex-col shadow-lg md:hover:scale-[102%] transition-all duration-300">
                 <Image
                   src={blog.blogImage}
                   width={2000}
@@ -216,14 +215,14 @@ function Blog() {
       </section>
 
 
-      <section className="trending-blogs mt-20 md:mr-24 mb-20">
+      <section className="trending-blogs mt-20  mb-20">
         <p className="md:text-4xl text-3xl">Trending <span className="text-first">Blogs</span></p>
         <p className="text-gray-500 text-lg font-light mt-1">Explore What Everyone's Talking About.</p>
         <Separator.Root orientation="horizontal" className="hidden md:block mx-0 my-4 w-20 lg:w-1/12 h-[1.8px] bg-third animate-shake" />
 
         <div className="boxes mt-10 md:flex gap-2">
 
-          <div className="md:h-[74vh] h-fit bg-white md:w-2/3 w-full md:shadow-lg shadow-md rounded-lg relative group md:hover:scale-[101%] duration-300 ">
+          <div className="h-fit bg-white md:w-2/3 w-full md:shadow-lg shadow-md rounded-lg relative group md:hover:scale-[101%] duration-300 ">
             {/* TAG AT THE TOP LEFT */}<p className="bg-first text-white w-fit px-2 py-1 rounded-br-lg rounded-tl-lg md:text-md text-sm mb-1 absolute">{trending1.tag}</p>
             <Link href={`/blogs/${trending1.slug}`}>
               <Image
