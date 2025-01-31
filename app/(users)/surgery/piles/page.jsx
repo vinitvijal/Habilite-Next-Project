41958b26a30690e7938b086ac726b0c4d0951fb6
Pancreass <mragankmuni000@gmail.com>
2025-01-31 22:19:29 +0530

    piles page done

--- app/(users)/surgery/piles/page.jsx
+++ app/(users)/surgery/piles/page.jsx
@@ -15,14 +15,14 @@ const features = [
 ];
 
 const FeatureList = ({ features }) => (
-  <div className="grid grid-cols-1 md:grid-cols-2 text-first md:text-lg text-left mx-auto">
+  <div className="grid grid-cols-1 md:grid-cols-2 text-first text-left mx-auto">
     {features.map((feature, index) => (
       <div
         key={index}
-        className={`flex items-start spaxe-x-2 py-3 rounded-lg  max-md:text-base border-third border-t ${index % 2 === 0 ? "md:mr-4" : "md:ml-4"} ${index===0?'border-none':''} ${index===1?'md:border-none':''}
+        className={`flex items-start space-x-2 py-2  max-md:text-base border-third border-t ${index % 2 === 0 ? "md:mr-4" : "md:ml-4"} ${index===0?'border-none':''} ${index===1?'md:border-none':''}
         }`} >
         <span> ➔ </span>
-        <p>{feature}</p>
+        <p className='max-sm:text-sm' >{feature}</p>
       </div>
     ))}
   </div>
@@ -174,8 +174,8 @@ const benefits = [
             variants={boxVariants}
             transition={{  duration: 1 }}
             whileInView="visible"
-            viewport={{ amount: 0.2 }}
-            className="w-3/5 m-4 mx-auto relative sm:-mt-24 lg:-mt-40 px-8 py-10 bg-white shadow-sm shadow-black items-center rounded-lg">
+            viewport={{ amount: 0.2,once:true }}
+            className="sm:w-2/3 w-full mx-auto relative sm:-mt-12 lg:-mt-24 md:px-6 py-4 bg-white sm:shadow-md items-center md:rounded-lg">
               <FeatureList features={features} />
             </motion.div>
           </div>
@@ -184,8 +184,8 @@ const benefits = [
             <motion.div 
             initial={{ opacity: 0, x: -100 }} 
             whileInView={{ opacity: 1, x: 0 }} 
-            viewport={{ amount: 0.3 }} 
-            transition={{ duration: 0.8 }}
+            viewport={{ amount: 0.3,once:true }} 
+            transition={{ duration: 0.6 }}
             className='pt-8'>
               <h2 className="text-2xl md:text-3xl font-semibold mb-2 mt-3 text-first"> Best Piles Treatment in Delhi</h2>
               <div className="mb-4">
@@ -203,7 +203,7 @@ const benefits = [
                     initial={{ opacity: 0 }}
                     whileInView={{ opacity: 1 }}
                     transition={{ duration: 1 }}
-                    viewport={{ amount: 0.2 }}
+                    viewport={{ amount: 0.2,once:true }}
                     className="text-3xl md:text-4xl  font-bold my-6 text-second"
                   >
                     {info.title}
@@ -211,8 +211,8 @@ const benefits = [
                   <motion.p
                     initial={{ opacity: 0 }}
                     whileInView={{ opacity: 1 }}
-                    transition={{ duration: 2 }}
-                    viewport={{ amount: 0.2 }}
+                    transition={{ duration: 1 }}
+                    viewport={{ amount: 0.2,once:true }}
                     className="text-lg sm:text-xl md:text-2xl text-left mb-8 text-first px-4 md:px-8 lg:px-16 mx-auto text">
                     {info.description}
                     <br/>
@@ -224,8 +224,8 @@ const benefits = [
             <motion.div 
             initial={{ opacity: 0, x: 100 }} 
             whileInView={{ opacity: 1, x: 0 }} 
-            viewport={{ amount: 0.3 }} 
-            transition={{ duration: 0.8 }}
+            viewport={{ amount: 0.1,once:true }} 
+            transition={{ duration: 0.6 }}
             className='mb-8'>           
               <div>
                 <h2 className="text-2xl md:text-3xl font-semibold mb-2 text-first"> What is Piles ?</h2>
@@ -267,8 +267,8 @@ const benefits = [
             <motion.div 
             initial={{ opacity: 0, x: 100 }} 
             whileInView={{ opacity: 1, x: 0 }} 
-            viewport={{ amount: 0.3 }} 
-            transition={{ duration: 0.8 }}>
+            viewport={{ amount: 0.3,once:true }} 
+            transition={{ duration: 0.6 }}>
               <div>
                 <h2 className="text-2xl md:text-3xl font-semibold mb-2 text-first">Symptoms of Piles</h2>
                 <div className="mb-4">
@@ -290,7 +290,7 @@ const benefits = [
             variants={imageVariants}
             initial="hidden"
             whileInView="visible"
-            viewport={{ amount: 0.2 }}>
+            viewport={{ amount: 0.2,once:true }}>
               <div className='mt-4 mb-8'>
                 <h2 className="text-2xl md:text-3xl font-semibold text-second text-left mb-4">BEST LASER PILES SURGERY IN DELHI</h2>
                 <div className="mb-4">        
@@ -320,7 +320,7 @@ const benefits = [
             variants={cardVariants}
             initial="hidden"
             whileInView="visible"
-            viewport={{ amount: 0.2 }}
+            viewport={{ amount: 0.2,once:true }}
             className="flex items-center justify-center max-w-6xl mx-auto">
               <div className=" p-12 border border-third hover:borounded-lg shadow-md">
                 <div className="mb-4">
@@ -350,8 +350,8 @@ const benefits = [
             <motion.div 
             initial={{ opacity: 0, x: 100 }} 
             whileInView={{ opacity: 1, x: 0 }} 
-            viewport={{ amount: 0.3 }} 
-            transition={{ duration: 0.8 }}>
+            viewport={{ amount: 0.2,once:true }} 
+            transition={{ duration: 0.6 }}>
               <div>
                 <h2 className="text-2xl md:text-3xl font-semibold mb-2 text-first">Stapler Surgery for Piles</h2>
                 <div className="mb-4">
@@ -386,7 +386,7 @@ const benefits = [
             <motion.div 
             initial={{ opacity: 0, x: -100 }} 
             whileInView={{ opacity: 1, x: 0 }} 
-            viewport={{ amount: 0.3 }} 
+            viewport={{ amount: 0.3,once:true }} 
             transition={{ duration: 0.8 }}>
               <h2 className="text-2xl md:text-3xl font-semibold mb-2 mt-3 text-first">
                 Why Choose Us - Best Hospital for Piles Treatment in Delhi
@@ -404,8 +404,8 @@ const benefits = [
           <div className="flex flex-col mx-auto pt-16 px-10 md:px-16 lg:px-24 gap-6">
             <motion.div initial={{ opacity: -2 }} 
             whileInView={{ opacity: 1.5 }} 
-            transition={{ duration: 2 }} 
-            viewport={{ amount: 0.2 }} 
+            transition={{ duration: 1 }} 
+            viewport={{ amount: 0.2,once:true }} 
             className='flex-col mx-6 mb-16 pt-4 md:px-24 border-first border-t-2'>
               <div className="flex justify-center items-end m-4"><BsPinFill className='text-3xl md:text-4xl text-second' />
                 <h1 className='font-bold text-3xl md:text-4xl text-center border-fourth border-b-2 mt-6 text-second'>FAQ</h1>
