3e6bfe50d7298f644c60a29d3d54dd10c6108588
Harshit <harshitkumar100436@gmail.com>
2025-01-31 17:26:25 +0530

    image and response added

--- app/(users)/about/dr-kapil-about/page.jsx
+++ app/(users)/about/dr-kapil-about/page.jsx
@@ -11,16 +11,24 @@ function DrKapilAbout() {
   const answer = "Dr. Kapil Agrawal specializes in advanced laparoscopic gastrointestinal surgeries and bariatric or weight-loss surgeries. He is renowned for his surgical precision and tailored treatment plans for patients.";
 
   return (
-    <div className="w-4/5 mx-auto px-4 py-8">
-      {/* Header Section */}
+    <div className="w-4/5 mx-auto px-4 pt-2 pb-8">
       <div className="text-center mb-8">
         <div className='relative flex flex-col items-center justify-center mb-5'>
+        <motion.div
+            initial={{ x: -100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.9, ease: "easeOut" }}
+          >
+          <div className='relative mb-5'>
+            <img src="/doctor.webp" alt="" width={350} height={300}/>
+          </div>
+          </motion.div>
           <motion.div
             initial={{ x: -100, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             transition={{ duration: 0.5, ease: "easeOut" }}
           >
-            <h1 className="text-3xl font-bold text-first mb-2 text-center">
+            <h1 className="text-xl md:text-3xl font-bold text-first mb-2 text-center">
               Dr. Kapil Agrawal – Best Bariatric Surgeon in Delhi
             </h1>
           </motion.div>
@@ -39,10 +47,10 @@ function DrKapilAbout() {
           transition={{ duration: 0.7, ease: "easeOut" }}
         >
           <div>
-            <h3 className="text-lg font-semibold text-gray-600 mb-1">
+            <h3 className="text-base md:text-lg font-semibold text-gray-600 mb-1">
               MBBS, MS (SURGERY), MRCS (LONDON, U.K), MMED (SINGAPORE), FMAS
             </h3>
-            <h3 className="text-lg text-gray-600">
+            <h3 className="text-base md:text-lg text-gray-600">
               Senior Consultant, Laparoscopic & Bariatric Surgeon
             </h3>
           </div>
@@ -56,13 +64,13 @@ function DrKapilAbout() {
         transition={{ duration: 0.9, ease: "easeOut" }}
       >
         <div className="mb-8">
-          <p className="text-gray-700 leading-relaxed mb-4">
+          <p className="text-sm md:text-base text-gray-700 leading-relaxed mb-4">
             Dr. Kapil Agrawal is one of the top choices for finding the best Bariatric, Metabolic, and Laparoscopic surgeons in Delhi, India. As one of the region's most respected Bariatric and Laparoscopic surgeons, he is well known for communicating effectively with his patients and utilizing innovative technology. He is also known for tailoring treatment plans to his patients' needs and surgical precision.
           </p>
-          <p className="text-gray-700 leading-relaxed mb-4">
+          <p className="text-sm md:text-base text-gray-700 leading-relaxed mb-4">
             He received his training at some of the best and most prominent surgical institutes in India and abroad. He was associated with North Middlesex University Hospital, Edmonton, U.K, and the Aintree University Hospital, Liverpool. He was honored with an MRCS (Member of Royal College of Surgeons, London, UK) in 2007 in England.
           </p>
-          <p className="text-gray-700 leading-relaxed">
+          <p className="text-sm md:text-base text-gray-700 leading-relaxed">
             His area of expertise includes advanced laparoscopic gastrointestinal surgeries as well as bariatric or weight-loss surgeries. Along with his team, he has helped thousands of patients with deflated self-esteem and inflated BMI, getting rid of obesity permanently.
           </p>
         </div>
@@ -74,7 +82,7 @@ function DrKapilAbout() {
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 1.1, ease: "easeOut" }}
         >
-          <h3 className="text-2xl font-semibold text-gray-800 mb-4">
+          <h3 className="text-base md:text-lg font-semibold text-gray-800 mb-4">
             A Brief Profile of Dr. Kapil Agarwal
           </h3>
         </motion.div>
@@ -83,16 +91,16 @@ function DrKapilAbout() {
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 1.3, ease: "easeOut" }}
         >
-          <p className="text-gray-700 leading-relaxed mb-4">
+          <p className="text-sm md:text-base text-gray-700 leading-relaxed mb-4">
             He is presently associated with Indraprastha Apollo Hospital, New Delhi, Apollo Spectra Hospital, Kailash Colony, New Delhi. Both hospitals are very well-established names in healthcare services. They are one of the most preferred destinations for Bariatric surgery and advanced laparoscopic surgeries in India. They are well known for offering top-notch services to patients coming from across the world.
           </p>
-          <p className="text-gray-700 leading-relaxed mb-4">
+          <p className="text-sm md:text-base text-gray-700 leading-relaxed mb-4">
             He has established a super specialty consultation centre, "HABILITE CLINICS," in South Delhi, intending to minimize the hospital's stressful experience and provide a warm and compassionate environment to his patients. The clinics offer a holistic program for the management of obesity and various advanced laparoscopic and laser surgeries to manage multiple ailments.
           </p>
-          <p className="text-gray-700 leading-relaxed mb-4">
+          <p className="text-sm md:text-base text-gray-700 leading-relaxed mb-4">
             Over the past several years, Dr. Kapil Agrawal has conducted many workshops, live surgeries, and OPDs in several African, Asian, and CIS countries. His interest in sharing advancements in laparoscopic and bariatric surgery has accorded him invitations to many National and International Conferences.
           </p>
-          <p className="text-gray-700 leading-relaxed">
+          <p className="text-sm md:text-base text-gray-700 leading-relaxed">
             He also has several research articles to his credit. He is a regular face on media channels. He is invited to share his expert opinion on obesity and related issues.
           </p>
         </motion.div>
