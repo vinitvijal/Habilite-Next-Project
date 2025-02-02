c28d3b45baf1582358b5a6b25869a97fd26b93a0
roshni <roshni.23545@sscbs.du.ac.in>
2025-02-02 16:24:33 +0530

    made minor changes

--- app/(users)/calculators/calorie/page.jsx
+++ app/(users)/calculators/calorie/page.jsx
@@ -5,26 +5,25 @@ import Head from 'next/head'
 
 function page() {
   return (
-    // <div className='h-full w-full justify-center items-center flex-col'>
-    //   <div className='w-full h-[6vh] justify-center items-center flex'>
-    //     <h1 className='font-bold text-5xl text-first'>Calorie Calculator</h1>
-    //   </div>
-    //   <div className='w-full justify-center items-center px-44 py-5'>
-    //     <div className='w-2/3  border-2'>
-    //         <h1>Gender:</h1>
-    //         <div className='flex'>
-    //             <button className='bg-third py-2 px-6 border-2 border-white/45 border-r-0 rounded-l-lg'>Male</button>
-    //             <button className='bg-third py-2 px-6 border-2 border-white/45 rounded-r-lg'>Female</button>
-    //         </div>
-    //     </div>
-    //   </div>
-    // </div>
-    <div className="min-h-screen mt-12 items-center justify-center ">
+  <div className="min-h-screen items-center justify-center ">
       <Head>
         <title>Calorie Calculator</title>
       </Head>
-      <main className="flex flex-col items-center justify-center w-full px-10 text-center">
-        <h1 className="text-5xl text-first font-bold mb-10">Calorie Calculator</h1>
+      <main className="flex flex-col items-center justify-center w-full text-center">
+        <div>
+          <div className="relative w-full pb-10">
+            <img
+              src="/banner-2.jpg"
+              alt="Background"
+              className="w-full object-cover opacity-200"/>
+        
+            <div className="absolute inset-0 flex items-center justify-center">
+              <h1 className="text-3xl md:text-5xl lg:text-7xl font-bold text-first px-4 py-2 rounded-md items-center justify-center text-center ">
+                Calorie Calculator
+              </h1>
+            </div>
+          </div>
+        </div>
         <CalorieCalculator/>
       </main>
     </div>
