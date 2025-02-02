c6aca03e32449cbeeb196c43762ee3f3909ddf4b
Ria Arora <riaarora@Rias-MacBook-Air.local>
2025-02-02 19:30:55 +0530

    changed bmi calc design

--- app/(users)/calculators/bmi/page.jsx
+++ app/(users)/calculators/bmi/page.jsx
@@ -9,8 +9,21 @@ function BmiPage() {
       <Head>
         <title>BMI Calculator</title>
       </Head>
-      <main className="flex flex-col items-center justify-center w-full px-10 text-center">
-        <h1 className="text-5xl text-first font-bold mb-10">BMI Calculator</h1>
+      <main>
+        <div>
+          <div className="relative w-full pb-10">
+            <img
+              src="/banner-2.jpg"
+              alt="Background"
+              className="w-full object-cover opacity-200"/>
+        
+            <div className="absolute inset-0 flex items-center justify-center">
+              <h1 className="text-3xl md:text-5xl lg:text-7xl font-bold text-first px-4 py-2 rounded-md items-center justify-center text-center ">
+                BMI Calculator
+              </h1>
+            </div>
+          </div>
+        </div>
         <BmiCalculator />
       </main>
     </div>
--- app/(users)/components/bmi.jsx
+++ app/(users)/components/bmi.jsx
@@ -2,110 +2,140 @@
 import React, { useState } from 'react';
 
 const BmiCalculator = () => {
-  const [gender, setGender] = useState('');
-  const [age, setAge] = useState('');
-  const [height, setHeight] = useState('');
-  const [weight, setWeight] = useState('');
-  const [bmi, setBmi] = useState(null);
-  const [message, setMessage] = useState('Enter your basic information to start calculating.');
+    const [gender, setGender] = useState('');
+    const [age, setAge] = useState('');
+    const [height, setHeight] = useState('');
+    const [weight, setWeight] = useState('');
+    const [bmi, setBmi] = useState(null);
+    const [message, setMessage] = useState('Enter your basic information to start calculating.');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!gender || !age || !height || !weight) {
+            setMessage('Please fill in all fields.');
+            return;
+        }
 
-    if (!gender || !age || !height || !weight) {
-      setMessage('Please fill in all fields.');
-      return;
-    }
+        const heightInMeters = height / 100;
+        const calculatedBmi = (weight / (heightInMeters * heightInMeters)).toFixed(2);
+        setBmi(calculatedBmi);
 
-    const heightInMeters = height / 100;
-    const calculatedBmi = (weight / (heightInMeters * heightInMeters)).toFixed(2);
-    setBmi(calculatedBmi);
+        let bmiMessage = `Your BMI is ${calculatedBmi}. `;
+        if (calculatedBmi < 16) {
+            bmiMessage += 'You are underweight.';
+        } else if (calculatedBmi < 22.5) {
+            bmiMessage += 'You have a healthy weight.';
+        } else if (calculatedBmi < 27.5) {
+            bmiMessage += 'You are overweight.';
+        } else if (calculatedBmi < 32.5) {
+            bmiMessage += 'You are obese.';
+        } else if (calculatedBmi < 37.5) {
+            bmiMessage += 'You are severely obese.';
+        } else {
+            bmiMessage += 'You are morbidly obese.';
+        }
+        setMessage(bmiMessage);
+    };
 
-    let bmiMessage = `Your BMI is ${calculatedBmi}. `;
-    if (calculatedBmi < 16) {
-      bmiMessage += 'You are underweight.';
-    } else if (calculatedBmi < 22.5) {
-      bmiMessage += 'You have a healthy weight.';
-    } else if (calculatedBmi < 27.5) {
-      bmiMessage += 'You are overweight.';
-    } else if (calculatedBmi < 32.5) {
-      bmiMessage += 'You are obese.';
-    } else if (calculatedBmi < 37.5) {
-      bmiMessage += 'You are severely obese.';
-    } else {
-      bmiMessage += 'You are morbidly obese.';
-    }
-    setMessage(bmiMessage);
-  };
-
-  return (
-    <div className="flex flex-col lg:flex-row justify-center items-center lg:gap-20 gap-10">
-      <div className="w-full max-w-lg p-8 bg-white rounded-lg shadow-lg border-2">
-        <form onSubmit={handleSubmit} className="flex flex-col space-y-5">
-          <div>
-            <label className="block text-md font-medium text-black">You are:</label>
-            <select value={gender} onChange={(e) => setGender(e.target.value)} className="mt-1 block w-full px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-second-500 focus:border-second-500 sm:text-md">
-              <option value="">Select Gender</option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-            </select>
-          </div>
-          <div>
-            <label className="block text-md font-medium text-black">How old are you?</label>
-            <input type="number" value={age} onChange={(e) => setAge(e.target.value)} className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-second-500 focus:border-second-500 sm:text-md" />
-          </div>
-          <div>
-            <label className="block text-md font-medium text-black">How tall are you? (cm)</label>
-            <input type="number" value={height} onChange={(e) => setHeight(e.target.value)} className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-second-500 focus:border-second-500 sm:text-md" />
-          </div>
-          <div>
-            <label className="block text-md font-medium text-black">How much do you weigh? (kg)</label>
-            <input type="number" value={weight} onChange={(e) => setWeight(e.target.value)} className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-second-500 focus:border-second-500 sm:text-md" />
-          </div>
-          <button type="submit" className="mt-4 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-md font-medium rounded-md text-white bg-first hover:bg-fourth focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-second">Calculate your BMI</button>
-        </form>
-
-        {message && <p className="mt-4 text-first text-sm">{message}</p>}
-      </div>
-
-      <div className="w-full max-w-lg bg-white rounded-lg shadow-lg border-2 flex justify-center items-center">
-          <table className="w-full text-left">
-            <thead className="bg-first text-white">
-              <tr>
-                <th className="border-b-2 border-gray-300 px-4 py-2 text-white text-md" style={{ width: '50%' }}>BMI Range</th>
-                <th className="border-b-2 border-gray-300 px-4 py-2 text-white text-md" style={{ width: '50%' }}>Category</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className="border-b border-gray-300 px-4 py-2">&lt; 16</td>
-                <td className="border-b border-gray-300 px-4 py-2">Underweight</td>
-              </tr>
-              <tr>
-                <td className="border-b border-gray-300 px-4 py-2">16 - 22.5</td>
-                <td className="border-b border-gray-300 px-4 py-2">Healthy weight</td>
-              </tr>
-              <tr>
-                <td className="border-b border-gray-300 px-4 py-2">22.5 - 27.5</td>
-                <td className="border-b border-gray-300 px-4 py-2">Overweight</td>
-              </tr>
-              <tr>
-                <td className="border-b border-gray-300 px-4 py-2">27.5 - 32.5</td>
-                <td className="border-b border-gray-300 px-4 py-2">Obese</td>
-              </tr>
-              <tr>
-                <td className="border-b border-gray-300 px-4 py-2">32.5 - 37.5</td>
-                <td className="border-b border-gray-300 px-4 py-2">Severely obese</td>
-              </tr>
-              <tr>
-                <td className="border-b border-gray-300 px-4 py-2">37.5 and over</td>
-                <td className="border-b border-gray-300 px-4 py-2">Morbidly obese</td>
-              </tr>
-            </tbody>
-          </table>
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex flex-col lg:flex-row justify-center items-center lg:gap-20 gap-10">
+            <div className="w-full max-w-lg p-8 bg-gradient-to-r from-second/80 to-fourth rounded-lg shadow-lg border-2 border-white bg-opacity-50">
+                <form onSubmit={handleSubmit} className="flex flex-col space-y-5">
+                    <div className="justify-center items-center min-w-fit">
+                        <label className="block text-lg font-semibold text-white">Select your gender:</label>
+                        <div className="flex">
+                            <button
+                                type="button"
+                                onClick={() => setGender('male')}
+                                className={`py-2 px-4 w-full border-2 border-black/10 border-r-0 rounded-l-lg transition-colors duration-300 ${gender === 'male' ? 'bg-first text-white' : 'bg-white text-black hover:bg-gray-200'}`}
+                            >
+                                Male
+                            </button>
+                            <button
+                                type="button"
+                                onClick={() => setGender('female')}
+                                className={`py-2 px-4 w-full border-2 border-black/10 rounded-r-lg transition-colors duration-300 ${gender === 'female' ? 'bg-first text-white' : 'bg-white text-black hover:bg-gray-200'}`}
+                            >
+                                Female
+                            </button>
+                        </div>
+                    </div>
+                    <div>
+                        <label className="block text-lg font-semibold text-white">How old are you?</label>
+                        <input
+                            type="number"
+                            value={age}
+                            onChange={(e) => setAge(e.target.value)}
+                            className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-lg bg-white text-black"
+                        />
+                    </div>
+                    <div>
+                        <label className="block text-lg font-semibold text-white">How tall are you? (cm)</label>
+                        <input
+                            type="number"
+                            value={height}
+                            onChange={(e) => setHeight(e.target.value)}
+                            className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-lg bg-white text-black"
+                        />
+                    </div>
+                    <div>
+                        <label className="block text-lg font-semibold text-white">How much do you weigh? (kg)</label>
+                        <input
+                            type="number"
+                            value={weight}
+                            onChange={(e) => setWeight(e.target.value)}
+                            className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-lg bg-white text-black"
+                        />
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                        <button
+                            type="submit"
+                            className="mt-4 inline-flex justify-center py-3 px-6 border border-transparent shadow-sm text-lg font-medium rounded-md text-black bg-white hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-transform transform hover:scale-101"
+                        >
+                            Calculate your BMI
+                        </button>
+                        {message && <p className="mt-4 text-white text-md text-center">{message}</p>}
+                    </div>
+                </form>
+            </div>
+            <div className="w-full max-w-lg bg-white rounded-lg shadow-lg border-2 flex justify-center items-center">
+                <table className="w-full text-left">
+                    <thead className="bg-gradient-to-r from-second/80 to-fourth text-white">
+                        <tr>
+                            <th className="border-b-2 border-gray-300 px-4 py-2 text-white text-lg" style={{ width: '50%' }}>BMI Range</th>
+                            <th className="border-b-2 border-gray-300 px-4 py-2 text-white text-lg" style={{ width: '50%' }}>Category</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="border-b border-gray-300 px-4 py-2">&lt; 16</td>
+                            <td className="border-b border-gray-300 px-4 py-2">Underweight</td>
+                        </tr>
+                        <tr>
+                            <td className="border-b border-gray-300 px-4 py-2">16 - 22.5</td>
+                            <td className="border-b border-gray-300 px-4 py-2">Healthy weight</td>
+                        </tr>
+                        <tr>
+                            <td className="border-b border-gray-300 px-4 py-2">22.5 - 27.5</td>
+                            <td className="border-b border-gray-300 px-4 py-2">Overweight</td>
+                        </tr>
+                        <tr>
+                            <td className="border-b border-gray-300 px-4 py-2">27.5 - 32.5</td>
+                            <td className="border-b border-gray-300 px-4 py-2">Obese</td>
+                        </tr>
+                        <tr>
+                            <td className="border-b border-gray-300 px-4 py-2">32.5 - 37.5</td>
+                            <td className="border-b border-gray-300 px-4 py-2">Severely obese</td>
+                        </tr>
+                        <tr>
+                            <td className="border-b border-gray-300 px-4 py-2">37.5 and over</td>
+                            <td className="border-b border-gray-300 px-4 py-2">Morbidly obese</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 };
 
 export default BmiCalculator;
