2563160947d26d7d177231f2450a78926810cb6b
Vinit (Code Vinu) Vijal <vineet.22566@sscbs.du.ac.in>
2025-02-16 11:03:04 +0000

    Blog Page Started

--- app/admin/(dash)/blogs/page.jsx
+++ app/admin/(dash)/blogs/page.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Plus } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { NewTestimonialDialog } from "./testimonial-modal";
+import {
+  createTestimonial,
+  deleteAdminTest,
+  getAdminTest,
+} from "@/actions/testimonial";
 
-function page() {
+function Page() {
+  const [isNewTestimonialOpen, setIsNewTestiomonialOpen] = useState(false);
+  const [tests, setTests] = useState([]);
+
+  async function getData() {
+    const token = localStorage.getItem("token");
+    const data = await getAdminTest(token);
+    if (!data) {
+      alert("Error in Validation");
+    }
+    setTests(data);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+
   return (
-    <div>
-      Blogs
+    <div className="container mx-auto py-10 overflow-y-auto h-screen">
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-2xl font-bold">Blogs</h1>
+        <Button onClick={() => setIsNewTestiomonialOpen(true)}>
+          <Plus className="mr-2 h-4 w-4" />
+          New Blog
+        </Button>
+      </div>
+      <div className=" grid grid-cols-2 gap-4">
+        
+      </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default Page;
