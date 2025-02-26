9dc358e7c6c27c39b7cbd73175b219e46b8a8edd
soubhagyacodes <soubhagya613mishra@gmail.com>
2025-02-26 06:21:27 +0530

    Completed Header Changes

--- app/(users)/components/Header.jsx
+++ app/(users)/components/Header.jsx
@@ -14,7 +14,7 @@ import { LinkedInLogoIcon } from '@radix-ui/react-icons'
 
 function Header() {
   const selected = "text-first before:w-full hover:text-first";
-  const selectedDropdown = "text-first before:w-10/12 hover:text-first before:bg-center";
+  const selectedDropdown = "text-first before:w-11/12 hover:text-first before:bg-center";
   const dash = "";
   const selectedAcc = "bg-first text-white data-open:bg-first";
   const pathname = usePathname();
@@ -231,7 +231,7 @@ function Header() {
                 </div>
               </div>
               <div className='hover:cursor-pointer group'>
-                <p className={cn("select-none h-20  relative flex items-center gap-1 rounded-3xl px-5 py-2 text-gray-500 hover:text-black transition-all duration-300 ease-in-out group-hover:text-first before:content-[''] before:bg-first before:w-0 before:absolute before:left-0 before:h-[2.5px] before:bottom-[0.7rem] before:rounded-md before:transition-all before:duration-300", pathname.startsWith("/about") && selectedDropdown)}>Laparoscopic Surgery <svg className="group-hover:rotate-180 group-hover:translate-y-[2px]" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path className="" d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg></p>
+                <p className={cn("select-none h-20  relative flex items-center gap-1 rounded-3xl px-5 py-2 text-gray-500 hover:text-black transition-all duration-300 ease-in-out group-hover:text-first before:content-[''] before:bg-first before:w-0 before:absolute before:left-0 before:h-[2.5px] before:bottom-[0.7rem] before:rounded-md before:transition-all before:duration-300", pathname.startsWith("/surgery/laparoscopic") && selectedDropdown)}>Laparoscopic Surgery <svg className="group-hover:rotate-180 group-hover:translate-y-[2px]" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path className="" d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg></p>
                 <div className="DropdownMenuContent group-hover:display-appear group hidden group-hover:block visible absolute bg-white w-48 z-50 translate-x-2 -translate-y-2 shadow-xl rounded-md">
                   <p className="DropdownMenuItem text-md h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
                     <Link href="/surgery/laparoscopic/gall-stone">Gall Stones</Link>
@@ -251,7 +251,7 @@ function Header() {
                 </div>
               </div>
               <div className='hover:cursor-pointer group'>
-                <p className={cn("select-none h-20  relative flex items-center gap-1 rounded-3xl px-5 py-2 text-gray-500 hover:text-black transition-all duration-300 ease-in-out group-hover:text-first before:content-[''] before:bg-first before:w-0 before:absolute before:left-0 before:h-[2.5px] before:bottom-[0.7rem] before:rounded-md before:transition-all before:duration-300", pathname.startsWith("/about") && selectedDropdown)}>Laser Surgery <svg className="group-hover:rotate-180 group-hover:translate-y-[2px]" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path className="" d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg></p>
+                <p className={cn("select-none h-20  relative flex items-center gap-1 rounded-3xl px-5 py-2 text-gray-500 hover:text-black transition-all duration-300 ease-in-out group-hover:text-first before:content-[''] before:bg-first before:w-0 before:absolute before:left-0 before:h-[2.5px] before:bottom-[0.7rem] before:rounded-md before:transition-all before:duration-300", pathname.startsWith("/surgery/laser") && selectedDropdown)}>Laser Surgery <svg className="group-hover:rotate-180 group-hover:translate-y-[2px]" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path className="" d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg></p>
                 <div className="DropdownMenuContent group-hover:display-appear group hidden group-hover:block visible absolute bg-white w-48 z-50 translate-x-2 -translate-y-2 shadow-xl rounded-md">
                   <p className="DropdownMenuItem text-md h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
                     <Link href="/surgery/laser/piles">Hemorrhoids/Piles</Link>
@@ -274,7 +274,7 @@ function Header() {
                 </div>
               </div>
               <div className='hover:cursor-pointer group'>
-                <p className={cn("select-none h-20  relative flex items-center gap-1 rounded-3xl px-5 py-2 text-gray-500 hover:text-black transition-all duration-300 ease-in-out group-hover:text-first before:content-[''] before:bg-first before:w-0 before:absolute before:left-0 before:h-[2.5px] before:bottom-[0.7rem] before:rounded-md before:transition-all before:duration-300", pathname.startsWith("/about") && selectedDropdown)}>Bariatrics <svg className="group-hover:rotate-180 group-hover:translate-y-[2px]" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path className="" d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg></p>
+                <p className={cn("select-none h-20  relative flex items-center gap-1 rounded-3xl px-5 py-2 text-gray-500 hover:text-black transition-all duration-300 ease-in-out group-hover:text-first before:content-[''] before:bg-first before:w-0 before:absolute before:left-0 before:h-[2.5px] before:bottom-[0.7rem] before:rounded-md before:transition-all before:duration-300", pathname.startsWith("/surgery/bariatrics") && selectedDropdown)}>Bariatrics <svg className="group-hover:rotate-180 group-hover:translate-y-[2px]" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path className="" d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg></p>
                 <div className="DropdownMenuContent group-hover:display-appear group hidden group-hover:block visible absolute bg-white w-56 z-50 translate-x-2 -translate-y-2 shadow-xl rounded-md">
                   <p className="DropdownMenuItem text-md h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
                     <Link href="/surgery/bariatrics/bariatric-surgery">Bariatric Surgery</Link>
@@ -290,87 +290,9 @@ function Header() {
                   </p>
                 </div>
               </div>
-              <div className='hover:cursor-pointer group'>
-                <p className={cn("select-none h-20 relative flex items-center gap-1 rounded-3xl px-5 py-2 text-gray-500 hover:text-black transition-all duration-300 ease-in-out group-hover:text-first before:content-[''] before:bg-first before:w-0 before:h-[2.5px] before:bottom-[0.7rem] before:absolute before:left-0 before:self-center before:rounded-md before:transition-all before:duration-300", pathname.startsWith("/surgery") && selectedDropdown)}>Bariatrics <svg className="group-hover:rotate-180 group-hover:translate-y-[2px]" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg></p>
-                <div className="DropdownMenuContent hidden hover:block  group-hover:block group-hover:display-appear absolute bg-white w-40 z-50 translate-x-2 -translate-y-2 shadow-2xl rounded-md">
-                  <div className="DropdownMenuItem relative group2 flex h-10 justify-between items-center pl-4 pr-2 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                    Laparoscopic
-                    <ChevronRight className='h-3' />
-                    <div className="subdropdown hidden group2-hover:block animate-custfadein py-1 hover:block absolute top-0 left-full -translate-x-1 bg-white whitespace-nowrap shadow-md rounded-md text-black">
-                      <p className="subDropdownMenuItem text-md h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                        <Link href="/surgery/gall-stone">Gall Stones</Link>
-                      </p>
-                      <p className="subDropdownMenuItem text-md h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                        <Link href="/surgery/hernia">Hernia</Link>
-                      </p>
-                      <p className="subDropdownMenuItem text-md h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                        <Link href="/surgery/rectal-prolapse">Rectal Prolapse</Link>
-                      </p>
-                      <p className="subDropdownMenuItem text-md h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                        <Link href="/surgery/appendix">Appendix</Link>
-                      </p>
-                      <p className="subDropdownMenuItem text-md h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                        <Link href="/surgery/gerd">Gerd</Link>
-                      </p>
-                    </div>
-                  </div>
-                  <div className="DropdownMenuItem relative group3 flex h-10 justify-between items-center pl-4 pr-2 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                    Laser Surgery
-                    <ChevronRight className='h-3' />
-                    <div className="subdropdown hidden group3-hover:block animate-custfadein hover:block absolute py-1 top-0 left-full -translate-x-1 bg-white whitespace-nowrap shadow-md rounded-md text-black">
-                      <p className="subDropdownMenuItem text-md h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                        <Link href="/surgery/piles">Hemorrhoids/Piles
-                        </Link>
-                      </p>
-                      <p className="subDropdownMenuItem text-md h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                        <Link href="/surgery/anal-fissure">Anal Fissure
-                        </Link>
-                      </p>
-                      <p className="subDropdownMenuItem text-md h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                        <Link href="/surgery/pilonidal-sinus">Pilonidal Sinus
-                        </Link>
-                      </p>
-                      <p className="subDropdownMenuItem text-md h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                        <Link href="/surgery/anal-fistula">Anal Fistula
-                        </Link>
-                      </p>
-                      <p className="subDropdownMenuItem text-md h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                        <Link href="/surgery/lipoma">Lipoma
-                        </Link>
-                      </p>
-                      <p className="subDropdownMenuItem text-md h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                        <Link href="/surgery/circumcision">Circumcision
-                        </Link>
-                      </p>
-                    </div>
-                  </div>
-                  <div className="DropdownMenuItem relative group4 flex h-10 justify-between items-center pl-4 pr-2 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                    Bariatrics
-                    <ChevronRight className='h-3' />
-                    <div className="subdropdown hidden group4-hover:block animate-custfadein py-1 hover:block absolute top-0 left-full -translate-x-1 bg-white shadow-md rounded-md text-black whitespace-nowrap">
-                      <p className="subDropdownMenuItem text-md h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                        <Link href="/surgery/bariatric-surgery">Bariatric Surgery
-                        </Link>
-                      </p>
-                      <p className="subDropdownMenuItem text-md h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                        <Link href="/surgery/medical-weight-loss-program">Non-Surgical Weight Loss Program
-                        </Link>
-                      </p>
-                      <p className="subDropdownMenuItem text-md h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                        <Link href="/surgery/surgery-for-diabetes">Surgery for Diabetes
-                        </Link>
-                      </p>
-                      <p className="subDropdownMenuItem text-md h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
-                        <Link href="/surgery/intraGastric-balloon">Intragastric Balloon
-                        </Link>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div htmlFor="contact" className='hover:cursor-pointer'>
-                <Link href="/contact-us">
-                  <p className={cn("select-none  relative before:content-[''] before:bg-first before:w-0 before:h-[2px] before:absolute before:left-0 before:bottom-[-10px] before:rounded-md hover:before:w-full before:transition-all before:duration-300 rounded-3xl px-5 py-2 text-gray-500 hover:text-black peer-checked:bg-opacity-45 transition-all duration-300 ease-in-out", pathname.startsWith("/contact-us") && selected)}>International Patient</p>
+              <div htmlFor="int-patient" className='hover:cursor-pointer'>
+                <Link href="/international-patient">
+                  <p className={cn("select-none  relative before:content-[''] before:bg-first before:w-0 before:h-[2px] before:absolute before:left-0 before:bottom-[-10px] before:rounded-md hover:before:w-full before:transition-all before:duration-300 rounded-3xl px-5 py-2 text-gray-500 hover:text-black peer-checked:bg-opacity-45 transition-all duration-300 ease-in-out", pathname.startsWith("/international") && selected)}>International Patient</p>
                 </Link>
               </div>
               <div className='hover:cursor-pointer'>
@@ -381,7 +303,7 @@ function Header() {
               <div className='hover:cursor-pointer group'>
                 <p className={cn("select-none before:content-[''] before:bg-first before:w-0 before:absolute before:left-0 before:h-[2.5px] before:bottom-[0.7rem] before:rounded-md before:transition-all before:duration-300 h-20 relative flex items-center gap-1 rounded-3xl px-5 py-2 text-gray-500 hover:text-black transition-all duration-300 ease-in-out group-hover:text-first", pathname.startsWith("/more") && selectedDropdown)}>More <svg className="group-hover:rotate-180 group-hover:translate-y-[2px]" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg></p>
                 <div className="DropdownMenuContent hidden hover:block group-hover:block group-hover:display-appear absolute bg-white w-48 z-50 translate-x-2 -translate-y-2 shadow-2xl rounded-md">
-                  <Link href='/more/international-patient'>
+                  <Link href='/more/contact-us'>
                     <p className="DropdownMenuItem h-10 flex items-center px-4 py-1 hover:text-first hover:cursor-pointer hover:translate-x-1 transition-all duration-150">
                       Contact Us
                     </p>
